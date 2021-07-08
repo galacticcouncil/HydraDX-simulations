61196d965461916e2d12630579dc6f739b42ec23
Jamsheed Shorish <jamsheed@shorishresearch.com>
2021-07-08 14:05:28 +0200

    updated initialize_liquidity & v2_asset_utils for Y, coeffs

--- model/initialize_liquidity.py
+++ model/initialize_liquidity.py
@@ -1,6 +1,26 @@
 print("running file: initialize_liquidity.py")
 
-initial_values = {'UNI_Qi': 20000, 'UNI_Ri': 59580, 'UNI_Si': 10000, 'UNI_Qj': 20000, 'UNI_Rj': 11824049, 'UNI_Sj': 10000, 'UNI_ij': 59580, 'UNI_ji': 11824049, 'UNI_Sij': 704476839420, 'Ri': 59580, 'Si': 10000, 'Rj': 11824049, 'Sj': 10000, 'Sq': 20000, 'Q': 40000, 'H': 40000}
+# JS July 8th, 2021: Assumes a = 1 for calculation of Cj, Y
+initial_values = {  'UNI_Qi': 20000, 
+                    'UNI_Ri': 59580, 
+                    'UNI_Si': 10000, 
+                    'UNI_Qj': 20000, 
+                    'UNI_Rj': 11824049, 
+                    'UNI_Sj': 10000, 
+                    'UNI_ij': 59580, 
+                    'UNI_ji': 11824049, 
+                    'UNI_Sij': 704476839420, 
+                    'Ri': 59580,
+                    'Ci':1, 
+                    'Si': 10000, 
+                    'Rj': 11824049,
+                    'Cj': 200,
+                    'Sj': 10000, 
+                    'Sq': 20000, 
+                    'Q': 40000, 
+                    'H': 40000,
+                    'Y': 29674.61606
+}
 C =1000000000000
 
 
