6570ed0549f0c92ddb357667acc792bd5eff5fad
KrisParuch <45070202+KrisParuch@users.noreply.github.com>
2021-08-10 14:12:56 +0200

    clean up v2_asset_utils

--- model/parts/v2_asset_utils.py
+++ model/parts/v2_asset_utils.py
@@ -22,8 +22,6 @@ class V2_Asset(): #args
                         'dP': 0,   # delta price
                         }}
         
-    #def __getitem__(self, item):
-    #    return getattr(self, item)
 
     def add_new_asset(self, asset_id, reserve, coefficient, price):
         """
@@ -46,14 +44,8 @@ class V2_Asset(): #args
         - S increased by delta_S>0
         - C increased by delta_C>0
         """ 
-        # JS July 8, 2021: corrected references to 'pool' attribute
-        # Si = self.get_share(asset_id) 
-        # Ci = self.get_coefficient(asset_id)
-        # Ri = self.get_reserve(asset_id)
-        #Y = prev_state['Y']
-        #a = params['a']
-        for key in self.pool.keys():
-            # print(self.pool.items()) 
+
+        for key in self.pool.keys():            
             if key == asset_id:
                 self.pool[key]['R'] += delta_R
                 self.pool[key]['S'] += delta_S
@@ -66,14 +58,11 @@ class V2_Asset(): #args
         - S decreased by delta_S>0
         - C decreased by delta_C>0
         """
-        # JS July 8, 2021: corrected references to 'pool' attribute
+        
         Si = self.get_share(asset_id) 
         Ci = self.get_coefficient(asset_id)
         Ri = self.get_reserve(asset_id)
-        # Y = prev_state['Y']
-        # a = params['a']
         for key in self.pool.keys():
-            # print(self.pool.items()) 
             if key == asset_id:
                 Si = self.pool[key]['S'] 
                 Ci = self.pool[key]['C']
@@ -83,11 +72,6 @@ class V2_Asset(): #args
                 self.pool[key]['C'] -= delta_C
                 self.pool[key]['S'] = Si * (Ri - delta_R) / Ri
                 self.pool[key]['C'] = Ci * ((Ri - delta_R) / Ri) ** (a+1)
-                #self.pool[key]['W'] += delta_W
-                # JS July 8, 2021: 'Y' is a pool constant and is not part of any individual asset
-                # The 'Y' pool constant is updated via a mechanism hub
-                # self.pool[key]['Y'] = ((Y ** (-a)) - Ci * (Ri ** (-a)) + Ci_plus * ((Ri - delta_R) ** (-a))) ** (- (1 / a))
-
 
     def q_to_r_pool(self, asset_id, delta_R):
         """
@@ -154,46 +138,7 @@ class V2_Asset(): #args
             if key == asset_id:
                 return(self.pool[key]['C'])
 
-    # JS July 8, 2021: this method is not used in the V2 Spec
-    ##########
-    # def update_price(self, Q, Wq):
-    #    """
-    #    updates price P of one specific asset from the pool variable and change in price dP for a Q and Sq according to
-    #    P = (Q/Sq)/(R/S)
-    #    dP = P - P
-    #    """        
-    #    for key in self.pool.keys():
-    #        R = self.pool[key]['R']
-    #        S = self.pool[key]['S']
-    #        P = self.pool[key]['P']
-    #        W = self.pool[key]['W']
-    #
-    #        self.pool[key]['P'] = (Q/Wq)/(R/W)
-    #        self.pool[key]['dP'] = self.pool[key]['P'] - P
-    ##########
-
-    # JS July 8, 2021: this method is not used in the V2 Spec
-    ##########
-    # def update_price_q_i(self, Ki, Q, Sq):
-    #    """
-    #    updates price according to mechanism specification from 3-3-21
-    #    """  
-    #    for key in self.pool.keys():
-    #        R = self.pool[key]['R']
-    #        S = self.pool[key]['S']
-    #        P = self.pool[key]['P']
-
-        ###############################################################################################
-        ########### YELLOW BOX HYDRA SPEC SWAP RISK ASSETS 3-3-21 #####################################
-        #    first_term = Ki / R
-        #    second_term_fraction = (Q / Sq) / (R / S)
-        #    price_q_i = first_term - second_term_fraction * np.log(R)
-        ########### YELLOW BOX HYDRA SPEC SWAP RISK ASSETS 3-3-21 #####################################
-        ###############################################################################################
-
-    #       self.pool[key]['P'] = price_q_i
-    ##########
-
+   
     # JS July 8, 2021: method updated according to V2 Spec
     def update_price_a(self, Q, Y, a):
         """
@@ -208,23 +153,8 @@ class V2_Asset(): #args
             self.pool[key]['P'] = (Q * Y**(a)) * (C / R**(a+1))
             self.pool[key]['dP'] = self.pool[key]['P'] - P
 
-            # print('POOL Class ', ' A value = ', a, ' Spot Price ',spot_price,'Class Price = ', self.pool['i']['P'])
-            # print('POOL Class ', ' A value = ', a, 'Asset ', key, ' Class Price = ', self.pool['i']['P'])
-        
-        # print('POOL Class ', ' A value = ', a, ' R over W= ', R/W,'Q over Wq = ', Q/Wq)
-
-    # JS July 8, 2021: this methiod is not used in the V2 Spec
-    ########## 
-    # def update_weight(self):
-    #    """
-    #    updates weight of specific asset from the pool variable according to
-    #    W = S
-    #    """ 
-    #    for key in self.pool.keys():
-    #        S = self.pool[key]['S']
-    #        self.pool[key]['W'] = S
-    ##########
-
+           
+   
     ## Balance Asset Share Swap
     def swap_share_pool(self, asset_id, delta_S):
         """
