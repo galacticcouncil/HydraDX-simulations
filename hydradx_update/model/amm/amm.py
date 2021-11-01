ee1429a3ec5bbb7fd8f0d4a5383ea6a5d9827c58
colin <grove.colin@gmail.com>
2021-11-01 17:30:27 -0500

    Fixed some bugs in AMM code

--- hydradx_update/model/amm/amm.py
+++ hydradx_update/model/amm/amm.py
@@ -1,3 +1,4 @@
+import copy
 import string
 
 from ..amm import omnipool_amm as oamm
@@ -50,6 +51,10 @@ def swap(old_state: dict, old_agents: dict, trade: dict) -> tuple:
         return oamm.swap_assets(old_state, old_agents, trade['agent_id'], trade['amount_sell'], i_buy, i_sell)
 
 
+def price_i(state: dict, i: int) -> float:
+    return oamm.price_i(state, i)
+
+
 def remove_liquidity(old_state: dict, old_agents: dict, transaction: dict) -> tuple:
     assert transaction['token_remove'] in old_state['token_list']
     agent_id = transaction['agent_id']
@@ -66,22 +71,16 @@ def add_liquidity(old_state: dict, old_agents: dict, transaction: dict) -> tuple
     return oamm.add_risk_liquidity(old_state, old_agents, agent_id, amount_add, i)
 
 
-def value_assets(state: dict, assets: dict, prices: list) -> float:
-    return assets['q'] + sum([assets['r'][i] * prices[i] for i in range(len(state['R']))])
+def value_assets(state: dict, assets: dict) -> float:
+    return assets['q'] + sum([assets['r'][i] * price_i(state, i) for i in range(len(state['R']))])
 
 
 def withdraw_all_liquidity(state: dict, agent_d: dict, agent_id: string) -> tuple:
-    agents_d = {agent_id: agent_d}
+    n = len(state['R'])
+    new_agents = {agent_id: agent_d}
+    new_state = copy.deepcopy(state)
 
-    transaction = {
-        'token_remove': 'HDX',
-        'agent_id': agent_id,
-        'shares_remove': -agent_d['h']
-    }
-
-    new_state, new_agents = remove_liquidity(state, agents_d, transaction)
-
-    for i in range(state['n']):
+    for i in range(n):
         transaction = {
             'token_remove': 'R' + str(i + 1),
             'agent_id': agent_id,
@@ -94,14 +93,14 @@ def withdraw_all_liquidity(state: dict, agent_d: dict, agent_id: string) -> tupl
 
 def value_holdings(state: dict, agent_d: dict, agent_id: string) -> float:
     new_state, new_agents = withdraw_all_liquidity(state, agent_d, agent_id)
-    return value_assets(new_state, new_agents[agent_id], state['P'])
+    return value_assets(new_state, new_agents[agent_id])
 
 
 def convert_agent(state: dict, agent_dict: dict) -> dict:
     """Return agent dict compatible with this amm"""
     n = len(state['R'])
     tks = state['token_list']
-    d = {'q': 0, 's': [0] * n, 'r': [0] * n}
+    d = {'q': 0, 's': [0] * n, 'r': [0] * n, 'p': [0] * n}
 
     # iterate through tokens held by AMM, look for both tokens and shares. Ignore the rest
     if 'HDX' in agent_dict:
@@ -111,6 +110,8 @@ def convert_agent(state: dict, agent_dict: dict) -> dict:
             d['r'][i] = agent_dict[tks[i]]
         if 'omni' + tks[i] in agent_dict:
             d['s'][i] = agent_dict['omni' + tks[i]]
+            # absent other information, assumes LPs contributed at current prices
+            d['p'][i] = price_i(state, i)
 
     return d
 
