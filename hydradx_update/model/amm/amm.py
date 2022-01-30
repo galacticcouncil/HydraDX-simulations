8239ad0d939cca90ee509d8d3704ebda3c656ab6
Colin <grove.colin@gmail.com>
2022-01-30 16:58:37 +0000

    Fixing bug in amm.py for trades for hub token (#34)
    closes #33

--- hydradx_update/model/amm/amm.py
+++ hydradx_update/model/amm/amm.py
@@ -66,7 +66,7 @@ def swap(old_state: dict, old_agents: dict, trade: dict) -> tuple:
         raise
 
     if i_buy < 0 or i_sell < 0:
-        return oamm.swap_lhdx_fee(old_state, old_agents, trade['agent_id'], trade_type, delta_R, delta_Q, max(i_buy, i_sell),
+        return oamm.swap_lhdx_fee(old_state, old_agents, trade['agent_id'], delta_R, delta_Q, max(i_buy, i_sell),
                                   old_state['fee_assets'],
                                   old_state['fee_HDX'])
     else:
