60c91d26f2b720a8d8942d7a3dc386ae544fcece
KrisParuch <45070202+KrisParuch@users.noreply.github.com>
2022-03-07 02:59:37 +0100

    some more tests

--- hydradx_update/model/model_initialization.py
+++ hydradx_update/model/model_initialization.py
@@ -31,6 +31,7 @@ def initialize_model(initial_lerna_in_pool, initial_tradevolume, initial_fee_ass
     LP4 = {'omniR4': initial_assets_in_pool[3]}
     LP5 = {'omniR5': initial_assets_in_pool[4]}
     
+    # following 'should' be okay as changes to def initialize_shares were made
     LP1 = {'omniR1': initial_lerna_in_pool[0]}
     LP2 = {'omniR2': initial_lerna_in_pool[1]}
     LP3 = {'omniR3': initial_lerna_in_pool[2]}
