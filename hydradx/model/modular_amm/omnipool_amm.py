b4ebfea874a87b9899c2b057ab56186c0e7d9edd
jepidoptera <jepidoptera@live.com>
2022-04-05 13:36:29 -0500

    add some checks

--- hydradx/model/modular_amm/omnipool_amm.py
+++ hydradx/model/modular_amm/omnipool_amm.py
@@ -53,22 +53,21 @@ class OmniPool(amm.Exchange):
         """ price of each asset denominated in LRNA """
         return self._asset_dict[index].lrna_price
 
-    def algebraic_symbols(self):
-        """ usage: P, Q, R, S, T, L, Q_total, T_total = Omnipool.algebraic_symbols() """
-        return self.P, self.Q, self.R, self.S, self.T, self.L, self.Q_total, self.T_total()
-
     @property
     def L(self):
         return self.lrnaImbalance
 
-    @L.setter
-    def L(self, value):
-        self.lrnaImbalance = value
+    def add_delta_L(self, value):
+        self.lrnaImbalance += value
 
     @property
     def C(self):
         """ soft cap on total value locked, denominated in preferred stablecoin """
         return self.tvlCapUSD
+
+    def algebraic_symbols(self):
+        """ usage: P, Q, R, S, T, L, Fp, Fa, Q_total, T_total = Omnipool.algebraic_symbols() """
+        return self.P, self.Q, self.R, self.S, self.T, self.L, self.lrnaFee, self.assetFee, self.Q_total, self.T_total()
 
 
 class OmnipoolAgent(amm.Agent):
@@ -88,7 +87,7 @@ class OmnipoolAgent(amm.Agent):
             self.add_liquidity(market, asset, quantity)
         else:
             self.external_market = market
-            super().add_asset(market=market, asset=asset, delta=quantity)
+        super().add_asset(market=market, asset=asset, quantity=quantity)
         return self
 
     def add_liquidity(self, market: amm.Exchange, asset: amm.Asset, quantity: float):
@@ -119,6 +118,7 @@ class OmnipoolAgent(amm.Agent):
         return self.holdings(self.omnipool, 'LRNA')
 
 
+# noinspection PyArgumentList
 def swap_assets(
         market_state: OmniPool,
         agents_list: list[OmnipoolAgent],
@@ -130,15 +130,19 @@ def swap_assets(
 
     i = sell_index
     j = buy_index
-    omni_pool = market_state
+    P, Q, R, S, T, L, Fp, Fa, Q_total, T_total = market_state.algebraic_symbols()
+    for n in [i, j]:
+        if not market_state.pool(n):
+            raise IndexError('asset {n} not found')
+    old_state = market_state
     assert sell_quantity > 0, 'sell amount must be greater than zero'
 
-    delta_Qi = omni_pool.Q(i) * -sell_quantity / (omni_pool.R(i) + sell_quantity)
-    delta_Qj = -delta_Qi * (1 - omni_pool.lrnaFee)
-    delta_Rj = omni_pool.R(j) * -delta_Qj / (omni_pool.Q(j) + delta_Qj) * (1 - omni_pool.assetFee)
-    delta_L = min(-delta_Qi * omni_pool.lrnaFee, -omni_pool.L)
-    delta_QH = -omni_pool.lrnaFee * delta_Qi - delta_L
     delta_Ri = sell_quantity
+    delta_Qi = Q(i) * -delta_Ri / (R(i) + delta_Ri)
+    delta_Qj = -delta_Qi * (1 - Fp)
+    delta_Rj = R(j) * -delta_Qj / (Q(j) + delta_Qj) * (1 - Fa)
+    delta_L = min(-delta_Qi * Fp, -L)
+    delta_QH = -delta_Qi * Fp - delta_L
 
     new_state = copy.deepcopy(market_state)
     new_state.add_delta_Q(i, delta_Qi)
@@ -146,11 +150,14 @@ def swap_assets(
     new_state.add_delta_R(i, delta_Ri)
     new_state.add_delta_R(j, delta_Rj)
     new_state.add_delta_Q('HDX', delta_QH)
-    new_state.L += delta_L
+    new_state.add_delta_L(delta_L)
 
     # do some algebraic checks
     if new_state.Q(i) * new_state.R(i) != pytest.approx(new_state.Q(i) * new_state.R(i)):
-        raise f'price change in asset {i}'
+        raise ValueError('price change in asset {i}')
+    if i != 0 and j != 0:
+        if delta_L + delta_Qj + delta_Qi + delta_QH != pytest.approx(0, abs=1e10):
+            raise ValueError('Some LRNA was lost along the way.')
 
     new_agents = copy.deepcopy(agents_list)
     trader = new_agents[trader_id]
@@ -170,13 +177,13 @@ def add_liquidity(market_state: OmniPool,
                   ) -> tuple[OmniPool, list[OmnipoolAgent]]:
     """ compute new state after agent[agent_index] adds liquidity to asset[asset_index] in quantity delta_r """
     if delta_r < 0:
-        raise "Cannot provision negative liquidity ^_-"
+        raise ValueError("Cannot provision negative liquidity ^_-")
     if agents_list[agent_index].r(asset_index) < delta_r:
         print('Transaction rejected because agent has insufficient funds.')
         print(f'(asset {asset_index}, agent {agent_index}, quantity {delta_r})')
         return market_state, agents_list
 
-    P, Q, R, S, T, L, Q_total, T_total = market_state.algebraic_symbols()
+    P, Q, R, S, T, L, Fp, Fa, Q_total, T_total = market_state.algebraic_symbols()
     U = market_state.stableCoin
     i = asset_index
     delta_q = Q(i) * delta_r / R(i)
@@ -184,20 +191,33 @@ def add_liquidity(market_state: OmniPool,
     delta_l = delta_r * Q(i) / R(i) * L / Q_total
     delta_t = (Q(i) + delta_q) * R(U) / Q(U) + T(i)
 
-    if T_total + delta_t > market_state.C:
+    if T_total + delta_t > market_state.tvlCapUSD:
         print('Transaction rejected because it would exceed allowable market cap.')
         print(f'(asset {asset_index}, agent {agent_index}), quantity {delta_r}')
         return market_state, agents_list
+
+    elif pytest.approx(R(i) / S(i)) != (R(i) + delta_r) / (S(i) + delta_s):
+        raise ValueError("Incorrect ratio of assets to shares.")
+
+    elif pytest.approx(Q(i) / R(i)) != (Q(i) + delta_q) / (R(i) + delta_r):
+        raise ValueError("Asset price should not change when liquidity is added.")
+
+    elif pytest.approx(Q(i) / R(i) * (Q_total + L) / Q_total) != \
+            (Q(i) + delta_q) / (R(i) + delta_r) * (Q_total + delta_q + L + delta_l) / (Q_total + delta_q):
+        # TODO: understand better what this means.
+        raise ValueError("Target price has changed.")
 
     new_state = copy.deepcopy(market_state)
     new_state.add_delta_Q(i, delta_q)
     new_state.add_delta_R(i, delta_r)
     new_state.add_delta_S(i, delta_s)
-    new_state.L += delta_l
+    new_state.add_delta_L(delta_l)
 
     new_agents = copy.deepcopy(agents_list)
     lp = new_agents[agent_index]
     lp.add_delta_s(delta_s)
+    lp.add_delta_r(-delta_r)
+    assert lp.p(i) == pytest.approx(new_state.price(i))
 
 
 # def add_asset(
