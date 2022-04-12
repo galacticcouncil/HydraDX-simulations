3f280d964fbe5bfb0a339e5338a3303f3f6ab448
jepidoptera <jepidoptera@live.com>
2022-04-11 19:22:25 -0500

    first test passing

--- hydradx/tests/test_modular_amm.py
+++ hydradx/tests/test_modular_amm.py
@@ -8,24 +8,25 @@ def test_market_construction():
     # noinspection SpellCheckingInspection
     hdx = amm.Asset(name='HDX', price=0.08)
     usd = amm.Asset(name='USD', price=1)
-    doge = amm.Asset(name='DOGE', price=0.000001)
+    doge = amm.Asset(name='DOGE', price=0.001)
     eth = amm.Asset(name='ETH', price=4000.0)
 
     omnipool = OmniPool(
         tvl_cap_usd=1000000,
         lrna_fee=0.001,
         asset_fee=0.002,
+        asset_list=[hdx, usd, doge, eth],
         preferred_stablecoin=usd
     )
-    omnipool.add_pool(hdx, 1000)
-    omnipool.add_pool(usd, 1000)
-    omnipool.add_pool(doge, 1000)
-    omnipool.add_pool(eth, 1000)
+    omnipool.add_lrna_pool(eth, 10)
+    omnipool.add_lrna_pool(usd, 100)
+    omnipool.add_lrna_pool(hdx, 1000)
+    omnipool.add_lrna_pool(doge, 100000)
 
     agents = [
         OmnipoolAgent(name='LP')
-        .add_position(omnipool.pool(doge).shareToken, 1000)
-        .add_position(omnipool.pool(hdx).shareToken, 1000),
+        .add_liquidity(omnipool, doge, 1000)
+        .add_liquidity(omnipool, hdx, 1000),
         OmnipoolAgent(name='trader')
         .add_position(usd, 1000)
         .add_position(eth, 1000),
@@ -88,7 +89,15 @@ def test_swap_asset(market_state, buy_index, sell_index, delta_r):
         .add_position(old_state.asset(buy_index), 0)
         .add_position(old_state.asset(sell_index), 1000000)
     ]
-    swap_assets(old_state, old_agents, sell_index, buy_index, trader_id=0, sell_quantity=delta_r)
+    new_state, new_agents = swap_assets(old_state, old_agents, sell_index, buy_index, trader_id=0, sell_quantity=delta_r)
+
+
+    # # do some algebraic checks
+    # if new_state.Q(i) * new_state.R(i) != pytest.approx(new_state.Q(i) * new_state.R(i)):
+    #     raise ValueError('price change in asset {i}')
+    # if i != 0 and j != 0:
+    #     if delta_L + delta_Qj + delta_Qi + delta_QH != pytest.approx(0, abs=1e10):
+    #         raise ValueError('Some LRNA was lost along the way.')
 
 
 @given(market_state=omnipool_config(tvl_cap_usd=1000000),
@@ -101,3 +110,14 @@ def test_add_liquidity(market_state, asset_index, quantity):
         .add_position(old_state.asset(asset_index), 10000000)
     ]
     add_liquidity(old_state, old_agents, agent_index=0, asset_index=asset_index, delta_r=quantity)
+
+    # if pytest.approx(R(i) / S(i)) != (R(i) + delta_r) / (S(i) + delta_s):
+    #     raise ValueError("Incorrect ratio of assets to shares.")
+    #
+    # elif pytest.approx(Q(i) / R(i)) != (Q(i) + delta_q) / (R(i) + delta_r):
+    #     raise ValueError("Asset price should not change when liquidity is added.")
+    #
+    # elif pytest.approx(Q(i) / R(i) * (Q_total + L) / Q_total) != \
+    #      (Q(i) + delta_q) / (R(i) + delta_r) * (Q_total + delta_q + L + delta_l) / (Q_total + delta_q):
+    #     # TODO: understand better what this means.
+    #     raise ValueError("Target price has changed.")
