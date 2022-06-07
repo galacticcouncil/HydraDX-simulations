c054eba261998bd1b3c44df5beaf8108ed40237a
jepidoptera <jepidoptera@live.com>
2022-06-07 11:19:09 -0500

    change 'plot' to 'plt'

--- hydradx/model/plot_utils.py
+++ hydradx/model/plot_utils.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import re
 
 
@@ -6,22 +6,22 @@ def plot_vars(df, var_list: list, sim_labels: list = ('0', '1')) -> None:
     simulations = df.simulation.unique()
     print(simulations)
     for var in var_list:
-        fig = plot.figure(figsize=(20, 5))
+        fig = plt.figure(figsize=(20, 5))
         if var in df.columns:
             # rows, columns, index
             bounds = (1, 3, 1)
-            ax = plot.subplot(*bounds, title=var)
+            ax = plt.subplot(*bounds, title=var)
             for i in simulations:
-                df[[var, 'timestep']][df['simulation'] == i].astype(float).plot(ax=ax, y=[var], x='timestep',
-                                                                                label=[sim_labels[i]])
+                df[[var, 'timestep']][df['simulation'] == i].astype(float).plt(ax=ax, y=[var], x='timestep',
+                                                                               label=[sim_labels[i]])
         else:
             matches = list(filter(lambda s: re.match(f'{var}-.+', s), df.columns))
 
             for i, label in enumerate(matches):
                 # rows, columns, index
                 bounds = (1, len(matches), i+1)
-                ax = plot.subplot(*bounds, title=label)
+                ax = plt.subplot(*bounds, title=label)
                 for j in simulations:
                     df[[label, 'timestep']][df['simulation'] == j]\
-                        .astype(float).plot(ax=ax, y=[label], x='timestep', label=[sim_labels[j]])
-    plot.show()
+                        .astype(float).plt(ax=ax, y=[label], x='timestep', label=[sim_labels[j]])
+    plt.show()
