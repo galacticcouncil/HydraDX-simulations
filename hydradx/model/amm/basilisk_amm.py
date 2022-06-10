574a5f40684b6d602300d5e8b2cdf7364fcea91b
jepidoptera <jepidoptera@live.com>
2022-06-10 16:50:51 -0500

    debug and refactor a bit

--- hydradx/model/amm/basilisk_amm.py
+++ hydradx/model/amm/basilisk_amm.py
@@ -1,6 +1,7 @@
 import copy
 import math
 
+
 class BasiliskPoolState:
     unique_ids = {''}
 
@@ -16,7 +17,7 @@ class BasiliskPoolState:
         self.trade_fee = trade_fee
         self.liquidity = dict()
         self.asset_list: list[str] = []
-        self.error = ''
+        self.fail = ''
 
         for token, quantity in tokens.items():
             self.asset_list.append(token)
@@ -32,8 +33,12 @@ class BasiliskPoolState:
 
     def copy(self):
         new_self = copy.deepcopy(self)
-        new_self.error = ''
+        new_self.fail = ''
         return new_self
+
+    @property
+    def invariant(self):
+        return math.prod(self.liquidity.values())
 
     def __repr__(self):
         return (
@@ -60,18 +65,21 @@ def add_risk_liquidity(
     new_state = old_state.copy()
 
     lp = new_agents[lp_id]
-    delta_r = {}
+    if new_state.unique_id not in lp['s']:
+        lp['s'][new_state.unique_id] = 0
+
     for token in old_state.asset_list:
-        delta_r[tkn_add] = quantity * new_state.liquidity[token] / new_state.liquidity[tkn_add]
-        lp['r'][token] -= delta_r[tkn_add]
-        new_state.liquidity[tkn_add] += delta_r
+        delta_r = quantity * old_state.liquidity[token] / old_state.liquidity[tkn_add]
+        lp['r'][token] -= delta_r
+        new_state.liquidity[token] += delta_r
 
         if lp['r'][token] < 0:
             # fail
             return fail(old_state, old_agents)
 
-    new_shares = new_state.liquidity[tkn_add] / old_state.liquidity[tkn_add] - 1
+    new_shares = (new_state.liquidity[tkn_add] / old_state.liquidity[tkn_add] - 1) * old_state.shares
     new_state.shares += new_shares
+
     lp['s'][new_state.unique_id] += new_shares
     return new_state, new_agents
 
@@ -81,13 +89,13 @@ def remove_liquidity(
         old_agents: dict,
         lp_id: str,
         quantity: float,
-        tkn: str
+        tkn_remove: str
 ) -> tuple[BasiliskPoolState, dict]:
-    quantity = abs(quantity)
+    quantity = abs(quantity) / old_state.shares * old_state.liquidity[tkn_remove]
     new_state, new_agents = add_risk_liquidity(
-        old_state, old_agents, lp_id, -quantity, tkn
+        old_state, old_agents, lp_id, -quantity, tkn_remove
     )  # does this work? test
-    if min(new_state.liquidity) < 0:
+    if min(new_state.liquidity.values()) < 0:
         return fail(old_state, old_agents)
 
     return new_state, new_agents
@@ -107,30 +115,27 @@ def swap(
     new_state = old_state.copy()
     trader = new_agents[trader_id]
 
-    invariant = math.prod(new_state.liquidity.values())
-
     if not (tkn_buy in new_state.asset_list and tkn_sell in new_state.asset_list):
         return fail(old_state, old_agents, 'invalid token name')
 
     if buy_quantity != 0:
-        sell_quantity = invariant / (new_state.liquidity[tkn_buy] - buy_quantity) - new_state.liquidity[tkn_sell]
+        new_state.liquidity[tkn_buy] -= buy_quantity
+        sell_quantity = old_state.invariant / new_state.liquidity[tkn_buy] - new_state.liquidity[tkn_sell]
         trade_fee = abs(sell_quantity) * new_state.trade_fee
-        trader['r'][tkn_sell] -= trade_fee
-        new_state.liquidity[tkn_sell] += trade_fee
+        trader['r'][tkn_sell] -= trade_fee + sell_quantity
+        trader['r'][tkn_buy] += buy_quantity
+        new_state.liquidity[tkn_sell] += trade_fee + sell_quantity
 
     elif sell_quantity != 0:
-        buy_quantity = new_state.liquidity[tkn_buy] - invariant / (new_state.liquidity[tkn_sell] + sell_quantity)
+        new_state.liquidity[tkn_sell] += sell_quantity
+        buy_quantity = new_state.liquidity[tkn_buy] - old_state.invariant / new_state.liquidity[tkn_sell]
         trade_fee = abs(buy_quantity) * new_state.trade_fee
-        trader['r'][tkn_buy] -= trade_fee
-        new_state.liquidity[tkn_buy] += trade_fee
+        trader['r'][tkn_buy] -= trade_fee - buy_quantity
+        trader['r'][tkn_sell] -= sell_quantity
+        new_state.liquidity[tkn_buy] += trade_fee - buy_quantity
 
     else:
         return fail(old_state, old_agents)
-
-    trader['r'][tkn_buy] += buy_quantity
-    trader['r'][tkn_sell] -= sell_quantity
-    new_state.liquidity[tkn_buy] -= buy_quantity
-    new_state.liquidity[tkn_sell] += sell_quantity
 
     if new_state.liquidity[tkn_buy] <= 0 or new_state.liquidity[tkn_sell] <= 0:
         return fail(old_state, old_agents)
@@ -143,5 +148,5 @@ def swap(
 
 def fail(old_state: BasiliskPoolState, old_agents: dict, error: str = 'fail') -> tuple[BasiliskPoolState, dict]:
     failed_state = old_state.copy()
-    failed_state.error = error
+    failed_state.fail = error
     return failed_state, copy.deepcopy(old_agents)
