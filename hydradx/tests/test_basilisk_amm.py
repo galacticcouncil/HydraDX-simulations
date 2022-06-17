a1220786df78cd7f708f9b079825f50d8acad3a7
jepidoptera <jepidoptera@live.com>
2022-06-17 16:42:35 -0500

    check swap reversibility (no fees) check remove liquidity separately

--- hydradx/tests/test_basilisk_amm.py
+++ hydradx/tests/test_basilisk_amm.py
@@ -1,9 +1,8 @@
-import copy
 import pytest
 import random
 from hypothesis import given, strategies as st, assume
 from hydradx.model.amm import basilisk_amm as bamm
-from hydradx.model.amm.amm import agent_dict
+from hydradx.model.amm.agents import agent_dict
 
 asset_price_strategy = st.floats(min_value=0.01, max_value=1000)
 asset_quantity_strategy = st.floats(min_value=1000, max_value=10000000)
@@ -22,25 +21,25 @@ def assets_config(draw) -> dict:
 
 
 @st.composite
-def basilisk_config(
+def constant_product_pool_config(
         draw,
         asset_dict=None,
         trade_fee=None,
-) -> bamm.BasiliskPoolState:
+) -> bamm.ConstantProductPoolState:
     asset_dict = asset_dict or draw(assets_config())
-    return bamm.BasiliskPoolState(
+    return bamm.ConstantProductPoolState(
         tokens=asset_dict,
         trade_fee=draw(st.floats(min_value=0, max_value=0.1)) if trade_fee is None else trade_fee,
     )
 
 
-@given(basilisk_config())
+@given(constant_product_pool_config())
 def test_basilisk_construction(initial_state):
-    assert isinstance(initial_state, bamm.BasiliskPoolState)
+    assert isinstance(initial_state, bamm.ConstantProductPoolState)
 
 
-@given(basilisk_config(), trade_quantity_strategy)
-def test_swap(initial_state: bamm.BasiliskPoolState, delta_r):
+@given(constant_product_pool_config(), trade_quantity_strategy)
+def test_swap(initial_state: bamm.ConstantProductPoolState, delta_r):
     old_state = initial_state
     trader = {
         'r': {token: 100 for token in initial_state.asset_list}
@@ -50,7 +49,7 @@ def test_swap(initial_state: bamm.BasiliskPoolState, delta_r):
     }
     tkn_sell = initial_state.asset_list[0]
     tkn_buy = initial_state.asset_list[1]
-    new_state, new_agents = bamm.swap(
+    swap_state, swap_agents = bamm.swap(
         old_state=old_state,
         old_agents=old_agents,
         trader_id='trader',
@@ -58,17 +57,18 @@ def test_swap(initial_state: bamm.BasiliskPoolState, delta_r):
         tkn_sell=tkn_sell,
         tkn_buy=tkn_buy
     )
-    if ((old_agents['trader']['r'][tkn_buy] + old_agents['trader']['r'][tkn_sell]
-         + old_state.liquidity[tkn_buy] + old_state.liquidity[tkn_sell])
-            != pytest.approx(new_agents['trader']['r'][tkn_buy] + new_agents['trader']['r'][tkn_sell]
-                             + new_state.liquidity[tkn_buy] + new_state.liquidity[tkn_sell])):
+    if (old_agents['trader']['r'][tkn_buy] + old_state.liquidity[tkn_buy]
+        != pytest.approx(swap_agents['trader']['r'][tkn_buy] + swap_state.liquidity[tkn_buy])
+        or old_state.liquidity[tkn_sell] + old_agents['trader']['r'][tkn_sell]
+        != pytest.approx(swap_state.liquidity[tkn_sell] + swap_agents['trader']['r'][tkn_sell])
+    ):
         raise AssertionError('Asset quantity is not constant after swap!')
 
     # swap back, specifying buy_quantity this time
-    delta_r = new_state.liquidity[tkn_sell] - old_state.liquidity[tkn_sell]
+    delta_r = swap_state.liquidity[tkn_sell] - old_state.liquidity[tkn_sell]
     revert_state, revert_agents = bamm.swap(
-        old_state=old_state,
-        old_agents=old_agents,
+        old_state=swap_state,
+        old_agents=swap_agents,
         trader_id='trader',
         buy_quantity=delta_r,
         tkn_sell=tkn_sell,
@@ -82,39 +82,57 @@ def test_swap(initial_state: bamm.BasiliskPoolState, delta_r):
         raise AssertionError('Asset quantity is not constant after swap!')
 
 
-@given(basilisk_config(trade_fee=0))
-def test_swap_pool_invariant(initial_state: bamm.BasiliskPoolState):
+@given(constant_product_pool_config(trade_fee=0), trade_quantity_strategy)
+def test_swap_pool_invariant(initial_state: bamm.ConstantProductPoolState, delta_r: float):
     old_state = initial_state
     trader = {
-        'r': {token: 100 for token in initial_state.asset_list}
+        'r': {token: 1000 for token in initial_state.asset_list}
     }
     old_agents = {
         'trader': trader
     }
     tkn_sell = initial_state.asset_list[0]
     tkn_buy = initial_state.asset_list[1]
-    new_state, new_agents = bamm.swap(
-        old_state,
-        old_agents,
-        'trader',
-        sell_quantity=100,
+    swap_state, swap_agents = bamm.swap(
+        old_state=old_state,
+        old_agents=old_agents,
+        trader_id='trader',
+        sell_quantity=delta_r,
         tkn_sell=tkn_sell,
         tkn_buy=tkn_buy
     )
     if ((old_state.liquidity[tkn_buy] * old_state.liquidity[tkn_sell])
-            != pytest.approx(new_state.liquidity[tkn_buy] * new_state.liquidity[tkn_sell])):
+            != pytest.approx(swap_state.liquidity[tkn_buy] * swap_state.liquidity[tkn_sell])):
         raise AssertionError('Pool invariant has varied.')
 
+    # swap back, specifying buy_quantity this time
+    delta_r = swap_state.liquidity[tkn_sell] - old_state.liquidity[tkn_sell]
+    revert_state, revert_agents = bamm.swap(
+        old_state=swap_state,
+        old_agents=swap_agents,
+        trader_id='trader',
+        buy_quantity=delta_r,
+        tkn_sell=tkn_buy,
+        tkn_buy=tkn_sell
+    )
+    # invariant should remain
+    if old_state.invariant != pytest.approx(revert_state.invariant):
+        raise AssertionError('Pool invariant has varied.')
 
-@given(basilisk_config(trade_fee=0), asset_quantity_strategy)
-def test_add_remove_liquidity(initial_state: bamm.BasiliskPoolState, delta_token: float):
+    if ((old_state.liquidity[tkn_buy] != pytest.approx(revert_state.liquidity[tkn_buy]))
+            or old_state.liquidity[tkn_sell] != pytest.approx(revert_state.liquidity[tkn_sell])):
+        raise AssertionError('Reverse sell with no fees yielded unexpected result')
+
+
+@given(constant_product_pool_config(trade_fee=0), asset_quantity_strategy)
+def test_add_remove_liquidity(initial_state: bamm.ConstantProductPoolState, delta_token: float):
     old_state = initial_state
     old_agents = {
         'lp': agent_dict(r_values={token: 1000000 for token in initial_state.asset_list})
     }
     tkn_add = initial_state.asset_list[0]
     other_tkn = initial_state.asset_list[1]
-    new_state, new_agents = bamm.add_risk_liquidity(
+    new_state, new_agents = bamm.add_liquidity(
         old_state, old_agents,
         lp_id='lp',
         quantity=delta_token,
@@ -144,9 +162,42 @@ def test_add_remove_liquidity(initial_state: bamm.BasiliskPoolState, delta_token
             or revert_state.shares != pytest.approx(old_state.shares)
             or revert_agents['lp']['r'][tkn_add] != pytest.approx(revert_agents['lp']['r'][tkn_add])
             or revert_agents['lp']['r'][other_tkn] != pytest.approx(revert_agents['lp']['r'][other_tkn])
-            or revert_agents['lp']['s'][old_state.unique_id] != pytest.approx(revert_agents['lp']['s'][old_state.unique_id])
+            or revert_agents['lp']['s'][old_state.unique_id] != pytest.approx(
+                revert_agents['lp']['s'][old_state.unique_id]
+            )
         ):
             raise AssertionError('Withdrawal failed to return to previous state.')
+
+
+@given(constant_product_pool_config(trade_fee=0), asset_quantity_strategy)
+def test_remove_liquidity(initial_state: bamm.ConstantProductPoolState, delta_token: float):
+    initial_agents = {
+        'lp': agent_dict(r_values={token: 1000000 for token in initial_state.asset_list})
+    }
+    tkn_remove = initial_state.asset_list[0]
+    # gotta add liquidity before we can remove it
+    old_state, old_agents = bamm.add_liquidity(
+        initial_state, initial_agents,
+        lp_id='lp',
+        quantity=delta_token,
+        tkn_add=tkn_remove
+    )
+    new_state, new_agents = bamm.remove_liquidity(
+        old_state, old_agents,
+        lp_id='lp',
+        quantity=delta_token,
+        tkn_remove=tkn_remove
+    )
+    other_tkn = initial_state.asset_list[1]
+    if (old_state.liquidity[tkn_remove] / old_state.liquidity[other_tkn]
+            != pytest.approx(new_state.liquidity[tkn_remove] / new_state.liquidity[other_tkn])):
+        raise AssertionError('Asset ratios not constant after liquidity remove!')
+
+    if ((old_agents['lp']['r'][tkn_remove] + old_agents['lp']['r'][other_tkn]
+         + old_state.liquidity[tkn_remove] + old_state.liquidity[other_tkn])
+            != pytest.approx(new_agents['lp']['r'][tkn_remove] + new_agents['lp']['r'][other_tkn]
+                             + new_state.liquidity[tkn_remove] + new_state.liquidity[other_tkn])):
+        raise AssertionError('Asset quantity is not constant after liquidity remove!')
 
 
 if __name__ == '__main__':
@@ -154,3 +205,4 @@ if __name__ == '__main__':
     test_swap()
     test_swap_pool_invariant()
     test_add_remove_liquidity()
+    test_remove_liquidity()
