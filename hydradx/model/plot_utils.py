dd59a48700aa02af6694fdaf1d16b3c112192615
jepidoptera <jepidoptera@live.com>
2022-07-07 16:59:10 -0500

    revamp: more specific graphs, more general model

--- hydradx/model/plot_utils.py
+++ hydradx/model/plot_utils.py
@@ -1,71 +1,123 @@
 import matplotlib.pyplot as plt
 
 
-def plot(events: list = None, pool: str = '', agent: str = '', asset: str = '', prop: str = ''):
-    datastream = [event['state'] for event in events]
-    keyword = ''
-    target = ''
+def plot(
+        events: list = None,
+        pool: str = '',
+        agent: str = '',
+        asset: str = '',
+        prop: str = '',
+        key: str = 'all',
+        subplot: plt.Subplot = None
+):
+    """
+    Given several specifiers, automatically create a graph or a series of graphs as appropriate.
+    Examples:
+        plot(events, pool='omnipool', prop='LRNA', key='R1'):
+            * plots LRNA in the R1/LRNA pool over time
+        plot(events, pool='all', prop='liquidity', key=['R1', 'R2'])
+            * plots R1 and R2 liquidity in all pools which have R1 or R2
+        plot(events, pool='R1/R2', prop='impermanent_loss', key='all')
+    """
     if pool:
-        keyword = pool
-        target = "pool"
-        datastream = [event['state'].pools for event in events]
+        group = "pools"
+        section = [pool]
+        label = "pool"
     elif agent:
-        keyword = agent
-        target = "agent"
-        datastream = [event['state'].agents for event in events]
+        group = "agents"
+        section = [agent]
+        label = "agent"
     elif asset:
-        keyword = asset
-        target = "asset"
-        datastream = [event['state'].external_market for event in events]
-
-    if not keyword:
+        group = "external_market"
+        section = [asset]
+        label = "asset price"
+    else:
         raise TypeError('plot() requires at least one of the following parameters: pool, agent, or asset.')
 
-    if keyword == 'all':
-        for key in datastream[0]:
-            plt.figure(figsize=(20, 5))
-            plot_stream(datastream, key, prop, label=f"{target} '{key}'")
-    elif keyword in datastream[0]:
-        plt.figure(figsize=(20, 5))
-        plot_stream(datastream, keyword, prop, label=f"{target} '{keyword}'")
-    else:
-        raise KeyError(f'{target} not found.')
+    if 'all' in [pool, agent, asset]:
+        section = [key for key in getattr(events[0]['state'], group)]
 
-
-def plot_stream(datastream: list, key: str = '', prop: str = '', label: str = '', subplot: plt.Subplot = None):
-    """
-    'datastream' should be in the form [{'agent1': <Agent>, 'agent2': <Agent>}, {'agent1: <Agent>'...]
-    'key' in this case would be 'agent1' or 'agent2'.
-    'prop' is the property of the object that we're graphing, such as 'holdings'.
-      - if the object is a number, this can be skipped
-    'label' is the prefix of the title to put on the graph: "agent 'agent1'" for instance.
-    """
-    if key:
-        # now we do a couple more list comprehensions to get it down to just a list of floats
-        first_step = datastream[0][key]
-        # first_step here should be dict of [str: object or float]
+    if key == 'all':
+        key = None
         if prop:
-            if hasattr(first_step, prop):
-                datastream = [getattr(step[key], prop) for step in datastream]
+            if isinstance(prop, str):
+                test_prop = getattr(getattr(events[0]['state'], group)[section[0]], prop)
+                if isinstance(test_prop, dict):
+                    # e.g. prop == liquidity, which is a dict. In this case we will graph all keys in the dict.
+                    keys = 'all'
+                else:
+                    # e.g. prop == market_cap, which is a float
+                    keys = ['']
             else:
-                raise AttributeError(f'"{type(datastream[0])}" object has no attribute {prop}.')
+                keys = ['']
         else:
-            datastream = [step[key] for step in datastream]
+            # e.g. asset is specified, meaning we don't need prop or key
+            keys = ['']
+    elif isinstance(key, list):
+        keys = key
+    else:
+        keys = [key]
 
-        first_step = datastream[0]
-        # first_step here should be either dict or float
+    if len(keys) == 1:
+        plt.figure(figsize=(20, 5))
 
-        if isinstance(first_step, dict):
-            # if it's a dict, we'll break it down into a list of floats and plot each separately
+    for instance in section:
+        if isinstance(prop, list):
+            use_props = prop
+        else:
+            use_props = [prop]
 
-            for i, k in enumerate(first_step):
-                bounds = (1, len(first_step.keys()), i + 1)
-                subplot = plt.subplot(*bounds, title=label + f' {prop}-{k}')
-                plot_stream(
-                    datastream=[step[k] for step in datastream],
-                    subplot=subplot
+        for p, prop in enumerate(use_props):
+            if keys == 'all':
+                use_keys = getattr(getattr(events[0]['state'], group)[instance], prop).keys()
+            else:
+                use_keys = keys
+
+            if len(use_keys) > 1 or keys == 'all':
+                # start a new line if we are doing multiple graphs at a time
+                plt.figure(figsize=(20, 5))
+
+            for k, key in enumerate(use_keys):
+                ax: plt.Subplot = subplot or plt.subplot(
+                    1, max(len(use_keys), len(use_props)), max(p, k)+1, title=f'{label}: {instance} {prop} {key}'
                 )
-            return
+                y = get_datastream(events=events, group=group, instance=instance, prop=prop, key=key)
+                x = range(len(y))
+                ax.plot(x, y)
 
-    ax: plt.Subplot = subplot or plt.subplot(1, 3, 1, title=f'{label} {prop}')
-    ax.plot(range(len(datastream)), datastream)
+
+def get_datastream(
+        events: list,
+        group: str = '',
+        instance: str = '',
+        pool: str = '',
+        agent: str = '',
+        asset: str = '',
+        prop: str = '',
+        key: str = ''
+) -> list[float]:
+    """
+    Takes entire events array and some specifiers as arguments.
+    Outputs one list of floats. Basically a list comprehension helper function.
+    """
+
+    if pool:
+        group = 'pools'
+        instance = pool
+    if agent:
+        group = 'agents'
+        instance = agent
+    if asset:
+        group = 'external_market'
+        key = asset
+
+    if not prop:
+        return [getattr(event['state'], group)[instance or key] for event in events]
+    if not key:
+        return [getattr(getattr(event['state'], group)[instance], prop) for event in events]
+
+    try:
+        return [getattr(getattr(event['state'], group)[instance], prop)[key] for event in events]
+    except KeyError:
+        # this may occur, for example, if a certain pool doesn't contain the assets specified by *key*
+        return []
