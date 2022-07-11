072401b3464afac36b413fa50d8d653a19e2aee0
jepidoptera <jepidoptera@live.com>
2022-07-11 13:36:00 -0500

    added market evolution step

--- hydradx/model/run.py
+++ hydradx/model/run.py
@@ -13,10 +13,13 @@ def run(initial_state: GlobalState, time_steps: int) -> list:
     print('Starting simulation...')
     for i in range(time_steps):
         new_global_state = events[-1]['state'].copy()
+        # agent actions
         agents = new_global_state.agents
         for agent_id, agent in agents.items():
             if agent.trade_strategy:
                 new_global_state = agent.trade_strategy.execute(new_global_state, agent.unique_id)
+        # market evolutions
+        new_global_state.evolve()
         events.append({'timestep': i, 'state': new_global_state})
 
     print(f'Execution time: {round(time.time() - start_time, 3)} seconds.')
