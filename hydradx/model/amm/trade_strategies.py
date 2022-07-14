2a0a4949afb0bb59d205be70d8e80e7e94e0cb5d
jepidoptera <jepidoptera@live.com>
2022-07-14 17:44:34 -0500

    Arbitrage needs to trade on the external market. Made it so.

--- hydradx/model/amm/trade_strategies.py
+++ hydradx/model/amm/trade_strategies.py
@@ -1,6 +1,6 @@
 import math
 
-from .global_state import GlobalState, swap, add_liquidity
+from .global_state import GlobalState, swap, add_liquidity, external_market_trade
 from .basilisk_amm import ConstantProductPoolState
 from typing import Callable
 import random
@@ -75,9 +75,7 @@ def constant_product_arbitrage(pool_id: str) -> TradeStrategy:
 
     def strategy(state: GlobalState, agent_id: str):
 
-        agent = state.agents[agent_id]
         pool = state.pools[pool_id]
-        old_pool = pool.copy()
         if not(isinstance(pool, ConstantProductPoolState)):
             raise TypeError(f'{pool_id} is not compatible with constant product arbitrage.')
 
@@ -85,9 +83,33 @@ def constant_product_arbitrage(pool_id: str) -> TradeStrategy:
         y = pool.asset_list[1]
         p = state.price(x) / state.price(y)
         y2 = math.sqrt(pool.liquidity[x] * pool.liquidity[y] * p)
-
         buy_quantity = pool.liquidity[y] - y2
-        state = swap(state, pool_id, agent_id, x, y, buy_quantity=buy_quantity)
-        return state
+
+        agent = state.agents[agent_id]
+        if x not in agent.holdings:
+            agent.holdings[x] = 0
+        if y not in agent.holdings:
+            agent.holdings[y] = 0
+
+        # buy just enough of non-USD asset
+        if buy_quantity > 0 and x != 'USD' or buy_quantity < 0 and y != 'USD':
+            state = external_market_trade(
+                state=state,
+                agent_id=agent_id,
+                tkn_buy=x if buy_quantity > 0 else 'USD',
+                tkn_sell=y if buy_quantity < 0 else 'USD',
+                sell_quantity=buy_quantity
+            )
+
+        # then swap
+        new_state = swap(state, pool_id, agent_id, tkn_sell=x, tkn_buy=y, buy_quantity=buy_quantity)
+
+        # immediately cash out everything for USD
+        new_agent = new_state.agents[agent_id]
+        for tkn, quantity in new_agent.holdings.items():
+            if new_agent.holdings[tkn] > 0 and tkn != 'USD':
+                new_state = external_market_trade(state, agent_id, tkn_buy='USD', tkn_sell=tkn, sell_quantity=quantity)
+
+        return new_state
 
     return TradeStrategy(strategy, name=f'invest all ({pool_id})')
