20c2d4b39e22c7a320ada947c22f3c723a94b227
jepidoptera <jepidoptera@live.com>
2022-09-08 17:39:31 -0500

    fix color_gradient

--- hydradx/model/plot_utils.py
+++ hydradx/model/plot_utils.py
@@ -155,12 +155,13 @@ def best_fit_line(data_array: list[float]):
     return [range(len(data_array)), [x * slope + intercept for x in range(len(data_array))]]
 
 
-def color_gradient(color1: tuple, color2: tuple, length):
+def color_gradient(color1: tuple, color2: tuple, length) -> list[str]:
     gradient = []
     for i in range(length):
-        gradient.append((
-            hex(int(color1[0] * (1 - i / length) + color2[0] * i / length))[2:].zfill(2),
-            hex(int(color1[1] * (1 - i / length) + color2[1] * i / length))[2:].zfill(2),
-            hex(int(color1[2] * (1 - i / length) + color2[2] * i / length))[2:].zfill(2),
+        gradient.append('#' + (
+                hex(int(color1[0] * (1 - i / length) + color2[0] * i / length))[2:].zfill(2) +
+                hex(int(color1[1] * (1 - i / length) + color2[1] * i / length))[2:].zfill(2) +
+                hex(int(color1[2] * (1 - i / length) + color2[2] * i / length))[2:].zfill(2)
         ))
     return gradient
+
