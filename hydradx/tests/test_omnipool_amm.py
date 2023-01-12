3cd665c89f421cbf41715435ab49d9a3d96ead54
poliwop <grove.colin@gmail.com>
2023-01-12 16:55:59 -0600

    Adjusted LRNA imbalance formula to follow new spec

--- hydradx/tests/test_omnipool_amm.py
+++ hydradx/tests/test_omnipool_amm.py
@@ -266,10 +266,12 @@ def test_swap_lrna(initial_state: oamm.OmnipoolState):
     if min(oamm.asset_invariant(new_state, i) / oamm.asset_invariant(old_state, i), 1) != pytest.approx(1):
         raise
 
-    if (new_state.liquidity[i] * old_state.lrna[i] *
-        new_state.lrna_total * (old_state.lrna_total + old_state.lrna_imbalance)) != pytest.approx(
-        old_state.liquidity[i] * new_state.lrna[i] *
-        old_state.lrna_total * (new_state.lrna_total + new_state.lrna_imbalance)
+    delta_qi = new_state.lrna[i] - old_state.lrna[i]
+    qi_arb = old_state.lrna[i] + delta_qi * old_state.lrna[i] / old_state.lrna_total
+    ri_arb = old_state.liquidity[i] * old_state.lrna_total / new_state.lrna_total
+
+    if ((old_state.lrna[i] + old_state.lrna_imbalance * (old_state.lrna[i] / old_state.lrna_total))*ri_arb) != pytest.approx(
+        (qi_arb + new_state.lrna_imbalance * (qi_arb / new_state.lrna_total))*old_state.liquidity[i]
     ):
         raise AssertionError(f'LRNA imbalance is wrong.')
 
@@ -649,15 +651,13 @@ def test_buy_stableswap_with_LRNA(initial_state: oamm.OmnipoolState):
     if (new_state.liquidity[stable_pool.unique_id] + stable_pool.shares
             != new_stable_pool.shares + initial_state.liquidity[stable_pool.unique_id]):
         raise AssertionError("Shares before and after trade don't add up.")
-    if (
-            (new_state.lrna[stable_pool.unique_id] + new_state.lrna_imbalance
-             * new_state.lrna[stable_pool.unique_id] / new_state.lrna_total)
-            * initial_state.liquidity[stable_pool.unique_id]
-            != pytest.approx(
-             (initial_state.lrna[stable_pool.unique_id] + initial_state.lrna_imbalance
-              * initial_state.lrna[stable_pool.unique_id] / initial_state.lrna_total)
-             * new_state.liquidity[stable_pool.unique_id]
-            )
+
+    delta_qi = new_state.lrna[stable_pool.unique_id] - initial_state.lrna[stable_pool.unique_id]
+    qi_arb = initial_state.lrna[stable_pool.unique_id] + delta_qi * initial_state.lrna[stable_pool.unique_id] / initial_state.lrna_total
+    ri_arb = initial_state.liquidity[stable_pool.unique_id] * initial_state.lrna_total / new_state.lrna_total
+
+    if ((initial_state.lrna[stable_pool.unique_id] + initial_state.lrna_imbalance * (initial_state.lrna[stable_pool.unique_id] / initial_state.lrna_total))*ri_arb) != pytest.approx(
+        (qi_arb + new_state.lrna_imbalance * (qi_arb / new_state.lrna_total))*initial_state.liquidity[stable_pool.unique_id]
     ):
         raise AssertionError("LRNA imbalance incorrect.")
     execution_price = (
