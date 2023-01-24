a644d056864cca4ea2616b74f25cb242ca2374e4
poliwop <grove.colin@gmail.com>
2023-01-24 17:57:28 -0600

    Extended new prices import method to monthly files

--- hydradx/model/processing.py
+++ hydradx/model/processing.py
@@ -147,3 +147,66 @@ def import_binance_prices(
             {tkn: price_data[tkn][i] for tkn in assets} for i in range(data_length)
         ]
     return price_data
+
+
+def import_monthly_binance_prices(
+    assets: list[str], start_month: str, months: int, interval: int = 12, return_as_dict: bool = False
+) -> dict[str: list[float]]:
+
+    start_mth, start_year = start_month.split(' ')
+
+    start_date = datetime.datetime.strptime(start_mth + ' 15 ' + start_year, "%b %d %Y")
+    dates = [datetime.datetime.strftime(start_date + datetime.timedelta(days=i*30), ("%Y-%m")) for i in range(months)]
+
+    # find the data folder
+    while not os.path.exists("./data"):
+        os.chdir("..")
+
+    # check that the files are all there, and if not, download them
+    for tkn in assets:
+        for date in dates:
+            file = f"{tkn}BUSD-1s-{date}"
+            if os.path.exists(f'./data/{file}.csv'):
+                continue
+            else:
+                print(f'Downloading {file}')
+                url = f"https://data.binance.vision/data/spot/monthly/klines/{tkn}BUSD/1s/{file}.zip"
+                response = requests.get(url)
+                with open(f'./data/{file}.zip', 'wb') as f:
+                    f.write(response.content)
+                with ZipFile(f"./data/{file}.zip", 'r') as zipObj:
+                    zipObj.extractall(path='./data')
+                os.remove(f"./data/{file}.zip")
+                # strip out everything except close price
+                with open(f'./data/{file}.csv', 'r') as input_file:
+                    rows = input_file.readlines()
+                with open(f'./data/{file}.csv', 'w', newline='') as output_file:
+                    output_file.write('\n'.join([row.split(',')[1] for row in rows]))
+
+    # now that we have all the files, read them in
+    price_data = {tkn: [] for tkn in assets}
+    for tkn in assets:
+        for date in dates:
+            file = f"{tkn}BUSD-1s-{date}"
+            with open(f'./data/{file}.csv', 'r') as input_file:
+                csvreader = reader(input_file)
+                price_data[tkn] += [float(row[0]) for row in csvreader][::interval]
+
+    if not return_as_dict:
+        price_data = [
+            {tkn: price_data[tkn][i] for tkn in assets} for i in range(len(price_data[assets[0]]))
+        ]
+    return price_data
+
+
+def import_prices(input_path: str, input_filename: str) -> list[PriceTick]:
+    price_data = []
+    with open(input_path + input_filename, newline='') as input_file:
+        fieldnames = ['timestamp', 'price']
+        reader = DictReader(input_file, fieldnames=fieldnames)
+        next(reader)  # skip header
+        for row in reader:
+            price_data.append(PriceTick(int(row["timestamp"]), float(row["price"])))
+
+    price_data.sort(key=lambda x: x.timestamp)
+    return price_data
