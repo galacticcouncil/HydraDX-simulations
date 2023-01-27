fc0e18e91382aac591481d931a834d19976ea3e5
poliwop <grove.colin@gmail.com>
2023-01-27 16:27:04 -0600

    - Extended cash_out_omnipool to allow agents to have LP shares but not the assets themselves - Fixed cash_out_omnipool to remove all shares from agent - added test test_cash_out_only_liquidity_at_spot

--- hydradx/tests/test_omnipool_state.py
+++ hydradx/tests/test_omnipool_state.py
@@ -4,6 +4,7 @@ from hydradx.model.amm import omnipool_amm as oamm
 from hydradx.model.amm.agents import Agent
 from hydradx.model.amm.omnipool_amm import OmnipoolState, value_assets, cash_out_omnipool
 from hydradx.tests.strategies_omnipool import reasonable_market_dict, omnipool_reasonable_config, reasonable_holdings
+from hydradx.tests.strategies_omnipool import reasonable_pct
 
 
 @given(reasonable_market_dict(token_count=5), reasonable_holdings(token_count=5))
@@ -15,11 +16,24 @@ def test_value_assets(market: dict, holdings: list):
         raise
 
 
-@given(omnipool_reasonable_config(asset_fee=0.0, lrna_fee=0.0, token_count=3), reasonable_market_dict(token_count=3), reasonable_holdings(token_count=3))
+@given(omnipool_reasonable_config(token_count=5), reasonable_market_dict(token_count=5), reasonable_holdings(token_count=5))
 def test_cash_out_no_liquidity(omnipool: OmnipoolState, market: dict, holdings: list):
     asset_list = list(market.keys())
     holdings_dict = {tkn: holdings[i] for i, tkn in enumerate(asset_list)}
     agent = Agent(holdings=holdings_dict)
     cash = cash_out_omnipool(omnipool, agent, market)
     if cash != sum([holdings_dict[tkn] * market[tkn] for tkn in asset_list]):
+        raise
+
+
+@given(omnipool_reasonable_config(token_count=5), reasonable_pct(token_count=5))
+def test_cash_out_only_liquidity_at_spot(omnipool: OmnipoolState, pct_list: list):
+    asset_list = omnipool.asset_list
+    holdings = {
+        (omnipool.unique_id, tkn): omnipool.liquidity[tkn] * pct_list[i] for i, tkn in enumerate(asset_list)
+    }
+    market = {tkn: omnipool.usd_price(tkn) for tkn in asset_list}
+    agent = Agent(holdings=holdings, share_prices={(omnipool.unique_id, tkn): omnipool.price(tkn) for tkn in asset_list})
+    cash = cash_out_omnipool(omnipool, agent, market)
+    if cash != sum([pct_list[i] * omnipool.liquidity[tkn] * market[tkn] for i, tkn in enumerate(asset_list)]):
         raise
