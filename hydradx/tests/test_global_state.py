5ead0ed30e4276938f8493d089c742acbd6b4a8c
poliwop <grove.colin@gmail.com>
2023-01-27 15:54:30 -0600

    - Moved cash_out_omnipool to omnpool_amm.py - Fixed but in cash_out_omnipool - Added tests

---
+++ hydradx/tests/test_global_state.py
@@ -0,0 +1,14 @@
+import pytest, random, copy
+from hypothesis import given, strategies as st, assume
+from hydradx.model.amm.global_state import value_assets
+from hydradx.tests.strategies_omnipool import reasonable_market_dict, reasonable_holdings
+
+
+# def value_assets(prices: dict, assets: dict) -> float:
+@given(reasonable_market_dict(token_count=5), reasonable_holdings(token_count=5))
+def test_value_assets(market: dict, holdings: list):
+    asset_list = list(market.keys())
+    assets = {asset_list[i]: holdings[i] for i in range(5)}
+    value = value_assets(market, assets)
+    if value != sum([holdings[i] * market[asset_list[i]] for i in range(5)]):
+        raise
