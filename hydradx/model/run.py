470755b149f30403bf587a08f5c1255366f2ae07
poliwop <grove.colin@gmail.com>
2023-01-31 15:51:41 -0600

    Restore possible use of cadCAD

--- hydradx/model/run.py
+++ hydradx/model/run.py
@@ -3,8 +3,8 @@ import time
 from .amm.global_state import GlobalState
 
 
-# from .cadCad import init_utils
-# from .cadCad.run import config as config_cadcad, run as run_cadcad
+from .cadCad import init_utils
+from .cadCad.run import config as config_cadcad, run as run_cadcad
 
 
 def run(initial_state: GlobalState, time_steps: int, silent: bool = False, use_cadcad: bool = False) -> list:
@@ -13,10 +13,10 @@ def run(initial_state: GlobalState, time_steps: int, silent: bool = False, use_c
     Run simulation
     """
 
-    # if use_cadcad:
-    #     config_dict = init_utils.get_configuration(time_steps)
-    #     config_cadcad(config_dict, {'state': initial_state})
-    #     return run_cadcad()[1:]
+    if use_cadcad:
+        config_dict = init_utils.get_configuration(time_steps)
+        config_cadcad(config_dict, {'state': initial_state})
+        return run_cadcad()[1:]
 
     start_time = time.time()
     events = [None] * time_steps
