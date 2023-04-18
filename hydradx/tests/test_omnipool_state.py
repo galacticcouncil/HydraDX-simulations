b29a0623f60f536236f94f9f5b35eb0ecb2a55fa
jepidoptera <39465008+jepidoptera@users.noreply.github.com>
2023-04-18 15:19:52 -0500

    Revert "Test workflows"

--- hydradx/tests/test_omnipool_state.py
+++ hydradx/tests/test_omnipool_state.py
@@ -12,31 +12,31 @@ from hydradx.tests.strategies_omnipool import reasonable_pct, asset_number_strat
 
 def test_omnipool_constructor_dynamic_fee_dict_works():
     omnipool = OmnipoolState(
-        tokens={
-            'HDX': {'liquidity': 1000000 / .05, 'LRNA': 1000000 / 20},
-            'USD': {'liquidity': 1000000, 'LRNA': 1000000 / 20},
-            'DOT': {'liquidity': 1000000 / 5, 'LRNA': 1000000 / 20},
-        },
-        oracles={'fee_raise': 50},  # , 'fee_lower': 7200},
-        lrna_fee=0.0005,
-        asset_fee={
-            'HDX': 0.0030,
-            'USD': dynamicadd_asset_fee(
-                minimum=0.0031,
-                amplification=0.2,
-                raise_oracle_name='fee_raise',
-                decay=0.00005,
-                fee_max=0.4,
-            ),
-            'DOT': dynamicadd_asset_fee(
-                minimum=0.0032,
-                amplification=0.2,
-                raise_oracle_name='fee_raise',
-                decay=0.00005,
-                fee_max=0.4,
-            ),
-        },
-    )
+            tokens={
+                'HDX': {'liquidity': 1000000/.05, 'LRNA': 1000000/20},
+                'USD': {'liquidity': 1000000, 'LRNA': 1000000/20},
+                'DOT': {'liquidity': 1000000/5, 'LRNA': 1000000/20},
+            },
+            oracles={'fee_raise': 50},  # , 'fee_lower': 7200},
+            lrna_fee=0.0005,
+            asset_fee={
+                'HDX': 0.0030,
+                'USD': dynamicadd_asset_fee(
+                    minimum=0.0031,
+                    amplification=0.2,
+                    raise_oracle_name='fee_raise',
+                    decay=0.00005,
+                    fee_max=0.4,
+                ),
+                'DOT': dynamicadd_asset_fee(
+                    minimum=0.0032,
+                    amplification=0.2,
+                    raise_oracle_name='fee_raise',
+                    decay=0.00005,
+                    fee_max=0.4,
+                ),
+            },
+        )
 
     assert omnipool.last_lrna_fee['HDX'] == 0.0
     assert omnipool.last_lrna_fee['USD'] == 0.0
@@ -55,37 +55,37 @@ def test_omnipool_constructor_dynamic_fee_dict_works():
 
 def test_omnipool_constructor_last_fee_works():
     omnipool = OmnipoolState(
-        tokens={
-            'HDX': {'liquidity': 1000000 / .05, 'LRNA': 1000000 / 20},
-            'USD': {'liquidity': 1000000, 'LRNA': 1000000 / 20},
-            'DOT': {'liquidity': 1000000 / 5, 'LRNA': 1000000 / 20},
-        },
-        oracles={'fee_raise': 50},
-        lrna_fee=0.0005,
-        asset_fee={
-            'HDX': 0.0030,
-            'USD': dynamicadd_asset_fee(
-                minimum=0.0031,
-                amplification=0.2,
-                raise_oracle_name='fee_raise',
-                decay=0.00005,
-                fee_max=0.4,
-            ),
-            'DOT': dynamicadd_asset_fee(
-                minimum=0.0032,
-                amplification=0.2,
-                raise_oracle_name='fee_raise',
-                decay=0.00005,
-                fee_max=0.4,
-            ),
-        },
-        last_lrna_fee=0.0005,
-        last_asset_fee={
-            'HDX': 0.0035,
-            'USD': 0.0036,
-            'DOT': 0.0037,
-        },
-    )
+            tokens={
+                'HDX': {'liquidity': 1000000/.05, 'LRNA': 1000000/20},
+                'USD': {'liquidity': 1000000, 'LRNA': 1000000/20},
+                'DOT': {'liquidity': 1000000/5, 'LRNA': 1000000/20},
+            },
+            oracles={'fee_raise': 50},
+            lrna_fee=0.0005,
+            asset_fee={
+                'HDX': 0.0030,
+                'USD': dynamicadd_asset_fee(
+                    minimum=0.0031,
+                    amplification=0.2,
+                    raise_oracle_name='fee_raise',
+                    decay=0.00005,
+                    fee_max=0.4,
+                ),
+                'DOT': dynamicadd_asset_fee(
+                    minimum=0.0032,
+                    amplification=0.2,
+                    raise_oracle_name='fee_raise',
+                    decay=0.00005,
+                    fee_max=0.4,
+                ),
+            },
+            last_lrna_fee=0.0005,
+            last_asset_fee={
+                'HDX': 0.0035,
+                'USD': 0.0036,
+                'DOT': 0.0037,
+            },
+        )
 
     assert omnipool.last_lrna_fee['HDX'] == 0.0005
     assert omnipool.last_lrna_fee['USD'] == 0.0005
@@ -97,53 +97,52 @@ def test_omnipool_constructor_last_fee_works():
 
 def test_constructor_oracle_from_block_works():
     omnipool = OmnipoolState(
-        tokens={
-            'HDX': {'liquidity': 1000000 / .05, 'LRNA': 1000000 / 20},
-            'USD': {'liquidity': 1000000, 'LRNA': 1000000 / 20},
-            'DOT': {'liquidity': 1000000 / 5, 'LRNA': 1000000 / 20},
-        },
-        oracles={'fee_raise': 50},
-        lrna_fee=0.0005,
-        asset_fee=0.0025,
-    )
+            tokens={
+                'HDX': {'liquidity': 1000000/.05, 'LRNA': 1000000/20},
+                'USD': {'liquidity': 1000000, 'LRNA': 1000000/20},
+                'DOT': {'liquidity': 1000000/5, 'LRNA': 1000000/20},
+            },
+            oracles={'fee_raise': 50},
+            lrna_fee=0.0005,
+            asset_fee=0.0025,
+        )
 
-    assert omnipool.oracles['fee_raise'].liquidity['HDX'] == pytest.approx(1000000 / .05, rel=1e-10)
-    assert omnipool.oracles['fee_raise'].price['HDX'] == pytest.approx(0.05 / 20, rel=1e-10)
+    assert omnipool.oracles['fee_raise'].liquidity['HDX'] == pytest.approx(1000000/.05, rel=1e-10)
+    assert omnipool.oracles['fee_raise'].price['HDX'] == pytest.approx(0.05/20, rel=1e-10)
     assert omnipool.oracles['fee_raise'].volume_in['HDX'] == 0.0
     assert omnipool.oracles['fee_raise'].volume_out['HDX'] == 0.0
 
 
 def test_constructor_last_oracle_values_works():
     omnipool = OmnipoolState(
-        tokens={
-            'HDX': {'liquidity': 1000000 / .05, 'LRNA': 1000000 / 20},
-            'USD': {'liquidity': 1000000, 'LRNA': 1000000 / 20},
-            'DOT': {'liquidity': 1000000 / 5, 'LRNA': 1000000 / 20},
-        },
-        oracles={'fee_raise': 50, 'test2': 100},
-        lrna_fee=0.0005,
-        asset_fee=0.0025,
-        last_oracle_values={
-            'fee_raise': {
-                'liquidity': {'HDX': 5000000, 'USD': 500000, 'DOT': 100000},
-                'volume_in': {'HDX': 10000, 'USD': 10000, 'DOT': 10000},
-                'volume_out': {'HDX': 10000, 'USD': 10000, 'DOT': 10000},
-                'price': {'HDX': 0.05, 'USD': 1, 'DOT': 5},
+            tokens={
+                'HDX': {'liquidity': 1000000/.05, 'LRNA': 1000000/20},
+                'USD': {'liquidity': 1000000, 'LRNA': 1000000/20},
+                'DOT': {'liquidity': 1000000/5, 'LRNA': 1000000/20},
             },
-            'test2': {
-                'liquidity': {'HDX': 5000000 * 1.1, 'USD': 500000 * 1.1, 'DOT': 100000 * 1.1},
-                'volume_in': {'HDX': 10000 * 1.1, 'USD': 10000 * 1.1, 'DOT': 10000 * 1.1},
-                'volume_out': {'HDX': 10000 * 1.1, 'USD': 10000 * 1.1, 'DOT': 10000 * 1.1},
-                'price': {'HDX': 0.05 * 1.1, 'USD': 1 * 1.1, 'DOT': 5 * 1.1},
+            oracles={'fee_raise': 50, 'test2': 100},
+            lrna_fee=0.0005,
+            asset_fee=0.0025,
+            last_oracle_values={
+                'fee_raise': {
+                    'liquidity': {'HDX': 5000000, 'USD': 500000, 'DOT': 100000},
+                    'volume_in': {'HDX': 10000, 'USD': 10000, 'DOT': 10000},
+                    'volume_out': {'HDX': 10000, 'USD': 10000, 'DOT': 10000},
+                    'price': {'HDX': 0.05, 'USD': 1, 'DOT': 5},
+                },
+                'test2': {
+                    'liquidity': {'HDX': 5000000*1.1, 'USD': 500000*1.1, 'DOT': 100000*1.1},
+                    'volume_in': {'HDX': 10000*1.1, 'USD': 10000*1.1, 'DOT': 10000*1.1},
+                    'volume_out': {'HDX': 10000*1.1, 'USD': 10000*1.1, 'DOT': 10000*1.1},
+                    'price': {'HDX': 0.05*1.1, 'USD': 1*1.1, 'DOT': 5*1.1},
+                }
             }
-        }
-    )
+        )
 
     assert omnipool.oracles['fee_raise'].liquidity['HDX'] == 5000000
-    assert omnipool.oracles['test2'].volume_in['USD'] == 10000 * 1.1
+    assert omnipool.oracles['test2'].volume_in['USD'] == 10000*1.1
     assert omnipool.oracles['fee_raise'].volume_out['DOT'] == 10000
-    assert omnipool.oracles['test2'].price['HDX'] == 0.05 * 1.1
-
+    assert omnipool.oracles['test2'].price['HDX'] == 0.05*1.1
 
 @given(reasonable_market_dict(token_count=5), reasonable_holdings(token_count=5))
 def test_value_assets(market: dict, holdings: list):
@@ -159,11 +158,10 @@ def test_value_assets(market: dict, holdings: list):
 def test_cash_out_no_liquidity(omnipool: OmnipoolState, market: dict, holdings: list):
     asset_list = list(market.keys())
     holdings_dict = {tkn: holdings[i] for i, tkn in enumerate(asset_list)}
-    agent = Agent(holdings=holdings_dict, )
+    agent = Agent(holdings=holdings_dict)
     cash = cash_out_omnipool(omnipool, agent, market)
     if cash != sum([holdings_dict[tkn] * market[tkn] for tkn in asset_list]):
-        raise AssertionError('Cash out failed: {} != {}'.format(
-            cash, sum([holdings_dict[tkn] * market[tkn] for tkn in asset_list])))
+        raise
 
 
 @given(omnipool_reasonable_config(token_count=5), reasonable_pct(token_count=5))
@@ -176,10 +174,7 @@ def test_cash_out_only_liquidity_at_spot(omnipool: OmnipoolState, pct_list: list
     agent = Agent(holdings=holdings,
                   share_prices={(omnipool.unique_id, tkn): oamm.price(omnipool, tkn) for tkn in asset_list})
     cash = cash_out_omnipool(omnipool, agent, market)
-    min_withdrawal_fee = 0.0001
-    res = sum([pct_list[i] * omnipool.liquidity[tkn] * market[tkn] * (1 - min_withdrawal_fee) for i, tkn in
-               enumerate(asset_list)])
-    if cash != pytest.approx(res, 10e-10):
+    if cash != sum([pct_list[i] * omnipool.liquidity[tkn] * market[tkn] for i, tkn in enumerate(asset_list)]):
         raise
 
 
@@ -227,41 +222,9 @@ def test_cash_out_one_asset_only_liquidity(omnipool: OmnipoolState, pct_list: li
     # xyk pool IL formula * initial assets LPed
     value_target = 2 * math.sqrt(k) / (k + 1) * initial_lp
     usd_price = oamm.usd_price(omnipool, held_asset)  # Need to convert from USD to TKN
-    # if cash / usd_price != pytest.approx(value_target, 1e-12):
-    #     raise
-
-    if cash / usd_price > value_target:
+    if cash / usd_price != pytest.approx(value_target, 1e-12):
         raise
 
     usd_value_target = 2 * math.sqrt(k_usd) / (k_usd + 1) * initial_usd_lp
-    # if cash_usdlp != pytest.approx(usd_value_target, 1e-12):
-    #     raise
-
-    if cash_usdlp > usd_value_target:
+    if cash_usdlp != pytest.approx(usd_value_target, 1e-12):
         raise
-
-
-@given(omnipool_reasonable_config())
-def test_cash_out_lrna(omnipool: OmnipoolState):
-    initial_lrna = 1000
-    agent = Agent(holdings={'LRNA': initial_lrna})
-    market = {tkn: oamm.usd_price(omnipool, tkn) for tkn in omnipool.asset_list}
-    cash = cash_out_omnipool(omnipool, agent, market)
-    if cash == 0:
-        raise ValueError("Cash out should not be zero")
-    # minus slippage, cash should be 1 / lrna_price('USD') * initial_lrna
-    if cash > 1 / oamm.lrna_price(omnipool, 'USD') * 1000:
-        raise ValueError("Cash out should not be greater than initial value")
-
-    omnipool.add_token(
-        tkn='newcoin',
-        liquidity=1000,
-        lrna=1000,
-        shares=1000,
-        protocol_shares=1000
-    )
-
-    market['newcoin'] = oamm.usd_price(omnipool, 'newcoin')
-
-    if cash_out_omnipool(omnipool, agent, market) <= cash:
-        raise ValueError("Cash out should be higher after adding new token")
