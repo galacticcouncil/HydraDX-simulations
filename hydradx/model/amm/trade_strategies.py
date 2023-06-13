cd1ab4157a41a7210ad41446b954335fe0eee759
jepidoptera <jepidoptera@live.com>
2023-06-13 12:04:02 -0500

    reverted change

--- hydradx/model/amm/trade_strategies.py
+++ hydradx/model/amm/trade_strategies.py
@@ -468,11 +468,11 @@ def omnipool_arbitrage(pool_id: str, arb_precision=1, skip_assets=None):
             low_price = (1 - usd_fee) * (1 - asset_LRNA_fee) * oamm.usd_price(omnipool, tkn=asset)
             high_price = 1 / (1 - asset_fee) / (1 - usd_LRNA_fee) * oamm.usd_price(omnipool, tkn=asset)
 
-            # if asset != omnipool.stablecoin and low_price <= state.external_market[asset] <= high_price:
-            #     skip_ct += 1
-            #     if i < usd_index:
-            #         usd_index -= 1
-            #     continue
+            if asset != omnipool.stablecoin and low_price <= state.external_market[asset] <= high_price:
+                skip_ct += 1
+                if i < usd_index:
+                    usd_index -= 1
+                continue
 
             reserves.append(omnipool.liquidity[asset])
             lrna.append(omnipool.lrna[asset])
