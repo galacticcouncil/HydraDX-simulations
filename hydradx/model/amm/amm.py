4e430e6cbba4819e94bac93e8b5d4d8276ff3da2
jepidoptera <jepidoptera@live.com>
2023-06-16 14:44:12 -0500

    re-enabled this bit about setting fees

--- hydradx/model/amm/amm.py
+++ hydradx/model/amm/amm.py
@@ -77,16 +77,16 @@ class AMM:
         self.fail = error
         return self, agent
 
-    # def __setattr__(self, key, value):
-    #     if hasattr(self, key):
-    #         if isinstance(self.__getattribute__(key), FeeMechanism):
-    #             if not isinstance(value, FeeMechanism):
-    #                 super().__setattr__(key, basic_fee(value))
-    #                 return
-    #             else:
-    #                 super().__setattr__(key, value.assign(self))
-    #                 return
-    #     super().__setattr__(key, value)
+    def __setattr__(self, key, value):
+        if hasattr(self, key):
+            if isinstance(self.__getattribute__(key), FeeMechanism):
+                if not isinstance(value, FeeMechanism):
+                    super().__setattr__(key, basic_fee(value))
+                    return
+                else:
+                    super().__setattr__(key, value.assign(self))
+                    return
+        super().__setattr__(key, value)
 
 
 def basic_fee(f: float = 0) -> FeeMechanism:
