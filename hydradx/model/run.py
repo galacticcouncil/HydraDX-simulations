6d048af158980e7b6f1038e6c6d3f9204a1e6277
poliwop <grove.colin@gmail.com>
2023-06-27 15:38:51 -0500

    Fixing run.py to address invest_all behavior

--- hydradx/model/run.py
+++ hydradx/model/run.py
@@ -22,10 +22,15 @@ def run(initial_state: GlobalState, time_steps: int, silent: bool = False) -> li
         new_global_state.evolve()
 
         # agent actions
-        agents = new_global_state.agents
-        for agent_id, agent in agents.items():
-            if agent.trade_strategy:
-                new_global_state = agent.trade_strategy.execute(new_global_state, agent.unique_id)
+        # agents = new_global_state.agents
+        # for agent_id, agent in agents.items():
+        #     if agent.trade_strategy:
+        #         new_global_state = agent.trade_strategy.execute(new_global_state, agent.unique_id)
+
+        for agent_id in new_global_state.agents:
+            # works as long as no trade_strategy adds or removes agents
+            if new_global_state.agents[agent_id].trade_strategy:
+                new_global_state = new_global_state.agents[agent_id].trade_strategy.execute(new_global_state, agent_id)
 
         events.append(new_global_state.archive())
 
