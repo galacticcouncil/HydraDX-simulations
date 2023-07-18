9b3cbe06ec3c36765096a60595f9b1b66c8e6447
poliwop <grove.colin@gmail.com>
2023-07-18 14:56:35 -0500

    Fixing invest_all

--- hydradx/model/run.py
+++ hydradx/model/run.py
@@ -1,6 +1,7 @@
 import time
 
 from .amm.global_state import GlobalState
+from copy import deepcopy
 
 
 def run(initial_state: GlobalState, time_steps: int, silent: bool = False) -> list:
@@ -22,11 +23,11 @@ def run(initial_state: GlobalState, time_steps: int, silent: bool = False) -> li
         new_global_state.evolve()
 
         # agent actions
-        agent_ids = list(new_global_state.agents.keys())
+        agent_ids = deepcopy(list(new_global_state.agents.keys()))
         for agent_id in agent_ids:
             agent = new_global_state.agents[agent_id]
             if agent.trade_strategy:
-                new_global_state = agent.trade_strategy.execute(new_global_state, agent_id)
+                new_global_state = agent.trade_strategy.execute(new_global_state, agent.unique_id)
 
         events.append(new_global_state.archive())
 
