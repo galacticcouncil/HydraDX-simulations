fd4c1ce80c2c2d3dc1286cf844e9c22b1fd15bfd
jepidoptera <jepidoptera@live.com>
2023-07-21 18:32:39 -0500

    undid change to amm.py

--- hydradx/model/amm/amm.py
+++ hydradx/model/amm/amm.py
@@ -64,15 +64,6 @@ class AMM:
     ):
         return self.copy(), old_agent.copy()
 
-    @staticmethod
-    def execute_add_liquidity(
-        state,
-        agent,
-        quantity: float,
-        tkn_add: str
-    ):
-        return state, agent
-
     def remove_liquidity(
         self,
         old_agent: Agent,
@@ -80,15 +71,6 @@ class AMM:
         tkn_remove: str
     ):
         return self.copy(), old_agent.copy()
-
-    @staticmethod
-    def execute_remove_liquidity(
-        state,
-        agent,
-        quantity: float,
-        tkn_add: str
-    ):
-        return state, agent
 
     def fail_transaction(self, error: str, agent: Agent):
         self.fail = error
