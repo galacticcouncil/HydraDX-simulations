cda5a4a74d37090b0ba5323b5c07f5ac94de51a4
jepidoptera <jepidoptera@live.com>
2023-09-11 15:37:53 -0500

    added postprocessing function back in for potential backwards compatibility

--- hydradx/model/processing.py
+++ hydradx/model/processing.py
@@ -5,6 +5,90 @@ import datetime
 import os
 
 
+from .amm.global_state import GlobalState, AMM, value_assets
+# from .amm.agents import Agent
+
+cash_out = GlobalState.cash_out
+impermanent_loss = GlobalState.impermanent_loss
+pool_val = GlobalState.pool_val
+deposit_val = GlobalState.deposit_val
+
+
+def postprocessing(events: list, optional_params: list[str] = ()) -> list:
+    """
+    Definition:
+    Compute more abstract metrics from the simulation
+
+    Optional parameters:
+    'withdraw_val': tracks the actual value of each agent's assets if they were withdrawn from the pool at each step
+    'deposit_val': tracks the theoretical value of each agent's original assets at each step's current spot prices,
+        if they had been held outside the pool from the beginning
+    'holdings_val': the total value of the agent's outside holdings
+    'pool_val': tracks the value of all assets held in the pool
+    'impermanent_loss': computes loss for LPs due to price movements in either direction
+    """
+    # save initial state
+    initial_state: GlobalState = events[0]
+    withdraw_state: GlobalState = initial_state.copy()
+
+    optional_params = set(optional_params)
+    if 'impermanent_loss' in optional_params:
+        optional_params.add('deposit_val')
+        optional_params.add('withdraw_val')
+
+    agent_params = {
+        'deposit_val',
+        'withdraw_val',
+        'holdings_val',
+        'impermanent_loss',
+        'token_count',
+        'trade_volume'
+    }
+    exchange_params = {
+        'pool_val',
+        # 'usd_price'
+    }
+    unrecognized_params = optional_params.difference(agent_params | exchange_params)
+    if unrecognized_params:
+        raise ValueError(f'Unrecognized parameter {unrecognized_params}')
+
+    for step in events:
+        state: GlobalState = step
+
+        for pool in state.pools.values():
+            if 'pool_val' in optional_params:
+                pool.pool_val = state.pool_val(pool)
+
+        # agents
+        for agent in state.agents.values():
+            if 'deposit_val' in optional_params:
+                # what are this agent's original holdings theoretically worth at current spot prices?
+                agent.deposit_val = value_assets(
+                    state.market_prices(agent.holdings),
+                    withdraw_state.agents[agent.unique_id].holdings
+                )
+            if 'withdraw_val' in optional_params:
+                # what are this agent's holdings worth if sold?
+                agent.withdraw_val = state.cash_out(agent)
+            if 'holdings_val' in optional_params:
+                agent.holdings_val = sum([quantity * state.price(asset) for asset, quantity in agent.holdings.items()])
+            if 'impermanent_loss' in optional_params:
+                agent.impermanent_loss = agent.withdraw_val / agent.deposit_val - 1
+            if 'token_count' in optional_params:
+                agent.token_count = sum(agent.holdings.values())
+            if 'trade_volume' in optional_params:
+                agent.trade_volume = 0
+                if state.time_step > 0:
+                    previous_agent = events[state.time_step - 1].agents[agent.unique_id]
+                    agent.trade_volume += (
+                        sum([
+                            abs(previous_agent.holdings[tkn] - agent.holdings[tkn]) * state.price(tkn)
+                            for tkn in agent.holdings])
+                    )
+
+    return events
+
+
 def import_binance_prices(
     assets: list[str], start_date: str, days: int, interval: int = 12,
     stablecoin: str = 'USDT', return_as_dict: bool = False
