aac71ffab242a41efdec1f13c2593e2551567874
poliwop <grove.colin@gmail.com>
2023-10-17 14:18:08 -0500

    Refactored out calculate_arb_amount_ask, test

--- hydradx/model/amm/arbitrage_agent.py
+++ hydradx/model/amm/arbitrage_agent.py
@@ -165,6 +165,60 @@ def calculate_arb_amount_bid(init_state: OmnipoolState, tkn, numeraire, bid: flo
     return amt
 
 
+def calculate_arb_amount_ask(init_state: OmnipoolState, tkn, numeraire, ask: float, cex_fee: float, min_amt = 1e-18, precision = 1e-15, max_iters=None):
+
+    state = init_state.copy()
+    agent = Agent(holdings={'USDT': 1000000000, 'DOT': 1000000000, 'HDX': 1000000000}, unique_id='bot')
+
+    asset_fee = state.asset_fee[tkn].compute(tkn=tkn)
+    lrna_fee = state.lrna_fee[numeraire].compute(tkn=numeraire)
+    cex_price = ask['price'] / (1 - cex_fee)
+
+    # If buying the min amount moves the price too much, return 0
+    if min_amt < 1e-18:
+        raise
+    test_agent = agent.copy()
+    test_state = state.copy()
+    test_state.swap(test_agent, tkn_buy=numeraire, tkn_sell=tkn, sell_quantity=min_amt)
+    op_spot = OmnipoolState.price(test_state, tkn, numeraire)
+    sell_spot = op_spot * (1 - lrna_fee) * (1 - asset_fee)
+    if min_amt >= state.liquidity[tkn] or sell_spot < cex_price or test_state.fail != '':
+        return 0
+
+    op_spot = OmnipoolState.price(state, tkn, numeraire)
+    sell_spot = op_spot * (1 - lrna_fee) * (1 - asset_fee)
+
+    # we use binary search to find the amount that can be swapped
+    amt_low = min_amt
+    amt_high = ask['amount']
+    amt = amt_high
+    i = 0
+    best_sell_spot = sell_spot
+    while best_sell_spot - cex_price > precision:
+        test_agent = agent.copy()
+        test_state = state.copy()
+        test_state.swap(test_agent, tkn_buy=numeraire, tkn_sell=tkn, sell_quantity=amt)
+        op_spot = OmnipoolState.price(test_state, tkn, numeraire)
+        sell_spot = op_spot * (1 - lrna_fee) * (1 - asset_fee)
+        if amt_high == amt_low:  # full amount can be traded
+            break
+        if sell_spot < cex_price or test_state.fail != '':
+            amt_high = amt
+        elif sell_spot > cex_price:
+            amt_low = amt
+            best_sell_spot = sell_spot
+
+        # only want to update amt if there will be another iteration
+        if best_sell_spot - cex_price > precision:
+            amt = amt_low + (amt_high - amt_low) / 2
+
+        i += 1
+        if max_iters is not None and i >= max_iters:
+            break
+
+    return amt
+
+
 def execute_arb(state, agent, all_swaps, cex_fee=0.0):
 
     for tkn_pair in all_swaps:
--- hydradx/tests/test_arbitrage_agent.py
+++ hydradx/tests/test_arbitrage_agent.py
@@ -1,7 +1,7 @@
 from hypothesis import given, strategies as st, settings, reproduce_failure
 
 from hydradx.model.amm.agents import Agent
-from hydradx.model.amm.arbitrage_agent import calculate_profit, calculate_arb_amount_bid
+from hydradx.model.amm.arbitrage_agent import calculate_profit, calculate_arb_amount_bid, calculate_arb_amount_ask
 from hydradx.model.amm.omnipool_amm import OmnipoolState
 
 
@@ -77,4 +77,66 @@ def test_calculate_arb_amount_bid(
         raise
 
     if amt == bid['amount'] and buy_spot > cex_price:
+        raise
+
+
+@given(
+    usdt_amt=st.floats(min_value=100000, max_value=1000000),
+    dot_price=st.floats(min_value=0.01, max_value=1000),
+    hdx_price=st.floats(min_value=0.01, max_value=1000),
+    dot_wt=st.floats(min_value=0.05, max_value=0.50),
+    hdx_wt=st.floats(min_value=0.01, max_value=0.20),
+    price_mult=st.floats(min_value=0.1, max_value=0.95),
+)
+def test_calculate_arb_amount_ask(
+        usdt_amt: float,
+        dot_price: float,
+        hdx_price: float,
+        dot_wt: float,
+        hdx_wt: float,
+        price_mult: float
+):
+
+    usdt_wt = 1 - dot_wt - hdx_wt
+    dot_lrna = dot_wt / usdt_wt * usdt_amt
+    dot_amt = dot_lrna / dot_price
+    hdx_lrna = hdx_wt / usdt_wt * usdt_amt
+    hdx_amt = hdx_lrna / hdx_price
+
+    tokens = {
+        'USDT': {'liquidity': usdt_amt, 'LRNA': usdt_amt},
+        'DOT': {'liquidity': dot_amt, 'LRNA': dot_lrna},
+        'HDX': {'liquidity': hdx_amt, 'LRNA': hdx_lrna}
+    }
+
+    lrna_fee = 0.0005
+    asset_fee = 0.0025
+    cex_fee = 0.0016
+
+    initial_state = OmnipoolState(
+        tokens=tokens,
+        lrna_fee=lrna_fee,
+        asset_fee=asset_fee,
+        preferred_stablecoin='USDT',
+    )
+
+    orig_price = initial_state.price(initial_state, 'DOT', 'USDT')
+    sell_spot = orig_price * ((1 - lrna_fee) * (1 - asset_fee))
+    ask_price = sell_spot * (1 - cex_fee) * price_mult
+
+    tkn = 'DOT'
+    numeraire = 'USDT'
+    ask = {'price': ask_price, 'amount': 100000}
+    p = 1e-10
+    amt = calculate_arb_amount_ask(initial_state, tkn, numeraire, ask, cex_fee, precision=p)
+    agent = Agent(holdings={'USDT': 1000000000, 'DOT': 1000000000, 'HDX': 1000000000}, unique_id='bot')
+    initial_state.swap(agent, tkn_buy=numeraire, tkn_sell=tkn, sell_quantity=amt)
+    test_price = initial_state.price(initial_state, tkn, numeraire)
+    sell_spot = test_price * ((1 - lrna_fee) * (1 - asset_fee))
+    cex_price = ask['price'] / (1 - cex_fee)
+
+    if abs(sell_spot - cex_price) > p and amt != ask['amount']:
+        raise
+
+    if amt == ask['amount'] and cex_price > sell_spot:
         raise
