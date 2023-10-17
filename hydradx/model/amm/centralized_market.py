ff5a6709db53ff6e4690adb73af314e7bb0f0dcb
jepidoptera <jepidoptera@live.com>
2023-10-17 13:03:57 -0500

    got everything straight

--- hydradx/model/amm/centralized_market.py
+++ hydradx/model/amm/centralized_market.py
@@ -1,10 +1,10 @@
 import copy
-from agents import Agent
-from amm import AMM
+from .agents import Agent
+from .amm import AMM
 
 
 class OrderBook:
-    def __init__(self, bids: list[tuple], asks: list[tuple]):
+    def __init__(self, bids: list[list], asks: list[list[float]]):
         """
         bids and asks are in the form of (price: float, quantity: float) tuples
         could add an ID in there later
@@ -18,13 +18,18 @@ class OrderBook:
 
 # faster
 OrderBook.copy = lambda self: OrderBook(
-    bids=[tuple(i for i in bid) for bid in self.bids],
-    asks=[tuple(i for i in ask) for ask in self.asks],
+    bids=[list(i for i in bid) for bid in self.bids],
+    asks=[list(i for i in ask) for ask in self.asks],
 )
 
 
 class CentralizedMarket(AMM):
-    def __init__(self, order_book: dict[tuple[str, str], OrderBook], asset_list: list[str] = None):
+    def __init__(
+            self,
+            order_book: dict[tuple[str, str], OrderBook],
+            asset_list: list[str] = None,
+            trade_fee: float = 0
+    ):
         super().__init__()
         self.order_book = order_book
         if asset_list:
@@ -34,6 +39,7 @@ class CentralizedMarket(AMM):
             self.asset_list = list({tkn: 0 for pair in self.order_book for tkn in pair}.keys())
         if 'USD' not in self.asset_list:
             self.asset_list = ['USD', *self.asset_list]
+        self.trade_fee = trade_fee
 
     def swap(
         self,
@@ -49,70 +55,104 @@ class CentralizedMarket(AMM):
             raise AssertionError('Asset not found in centralized market.')
 
         if self.asset_list.index(tkn_sell) < self.asset_list.index(tkn_buy):
-            quote = tkn_sell
             base = tkn_buy
+            quote = tkn_sell
         else:
-            quote = tkn_buy
             base = tkn_sell
+            quote = tkn_buy
 
         if (base, quote) not in self.order_book:
-            return self.fail_transaction('Order book not found.')
+            if (quote, base) in self.order_book:
+                base, quote = quote, base
+            else:
+                return self.fail_transaction('Order book not found.')
 
-        # make sure bids are sorted by price ascending and asks are sorted by price descending
-        self.order_book[(base, quote)].bids.sort(key=lambda x: x[0])
-        self.order_book[(base, quote)].asks.sort(key=lambda x: x[0], reverse=True)
+        # make sure asks are sorted by price ascending and bids are sorted by price descending
+        self.order_book[(base, quote)].asks.sort(key=lambda x: x[0])
+        self.order_book[(base, quote)].bids.sort(key=lambda x: x[0], reverse=True)
 
         if sell_quantity > 0:
-            if tkn_sell == base:
-                orders = self.order_book[(base, quote)].bids
+            if tkn_sell == quote:
+                sell_tkns_remaining = sell_quantity
+                tkns_bought = 0
+
+                for bid in self.order_book[(base, quote)].bids:
+                    if sell_tkns_remaining <= 0:
+                        break
+                    if bid[0] * bid[1] >= sell_tkns_remaining:
+                        # this bid can fill the entire remaining order
+                        tkns_bought += sell_tkns_remaining / bid[0]
+                        bid[1] -= sell_tkns_remaining / bid[0]
+                        sell_tkns_remaining = 0
+                    else:
+                        # this bid can partially fill the order
+                        tkns_bought += bid[1]
+                        sell_tkns_remaining -= bid[0] * bid[1]
+                        bid[1] = 0
+
+                agent.holdings[tkn_sell] -= sell_quantity - sell_tkns_remaining
+                agent.holdings[tkn_buy] += tkns_bought / (1 - self.trade_fee)
+                self.order_book[(base, quote)].bids = [bid for bid in self.order_book[(base, quote)].bids if bid[1] > 0]
             else:
-                orders = self.order_book[(base, quote)].asks
+                sell_tkns_remaining = sell_quantity
+                tkns_bought = 0
 
-            sell_tkns_remaining = sell_quantity
-            tkns_bought = 0
+                for ask in self.order_book[(base, quote)].asks:
+                    if sell_tkns_remaining <= 0:
+                        break
+                    if ask[1] >= sell_tkns_remaining:
+                        tkns_bought += ask[0] * sell_tkns_remaining
+                        ask[1] -= sell_tkns_remaining
+                        sell_tkns_remaining = 0
+                    else:
+                        tkns_bought += ask[0] * ask[1]
+                        sell_tkns_remaining -= ask[1]
+                        ask[1] = 0
 
-            for bid in orders:
-                if sell_tkns_remaining <= 0:
-                    break
-                if bid[1] >= sell_tkns_remaining:
-                    # this bid can fill the entire remaining order
-                    tkns_bought += bid[0] * sell_tkns_remaining
-                    bid[1] -= sell_tkns_remaining
-                    sell_tkns_remaining = 0
-                else:
-                    # this bid can partially fill the order
-                    tkns_bought += bid[0] * bid[1]
-                    sell_tkns_remaining -= bid[1]
-                    orders.remove(bid)
-
-            agent.holdings[tkn_sell] -= sell_quantity - sell_tkns_remaining
-            agent.holdings[tkn_buy] += tkns_bought
+                agent.holdings[tkn_sell] -= sell_quantity - sell_tkns_remaining
+                agent.holdings[tkn_buy] += tkns_bought / (1 - self.trade_fee)
+                self.order_book[(base, quote)].asks = [ask for ask in self.order_book[(base, quote)].asks if ask[1] > 0]
 
         elif buy_quantity > 0:
-            if tkn_buy == base:
-                orders = self.order_book[(base, quote)].asks
+            if tkn_buy == quote:
+                buy_tkns_remaining = buy_quantity
+                tkns_sold = 0
+
+                for ask in self.order_book[(base, quote)].asks:
+                    if buy_tkns_remaining <= 0:
+                        break
+                    if ask[0] * ask[1] >= buy_tkns_remaining:
+                        tkns_sold += buy_tkns_remaining / ask[0]
+                        ask[1] -= buy_tkns_remaining / ask[0]
+                        buy_tkns_remaining = 0
+                    else:
+                        tkns_sold += ask[1]
+                        buy_tkns_remaining -= ask[0] * ask[1]
+                        ask[1] = 0
+
+                agent.holdings[tkn_buy] += buy_quantity - buy_tkns_remaining
+                agent.holdings[tkn_sell] -= tkns_sold * (1 - self.trade_fee)
+                self.order_book[(base, quote)].asks = [ask for ask in self.order_book[(base, quote)].asks if ask[1] > 0]
+
             else:
-                orders = self.order_book[(base, quote)].bids
+                buy_tkns_remaining = buy_quantity
+                tkns_sold = 0
 
-            buy_tkns_remaining = buy_quantity
-            tkns_sold = 0
+                for bid in self.order_book[(base, quote)].bids:
+                    if buy_tkns_remaining <= 0:
+                        break
+                    if bid[1] >= buy_tkns_remaining:
+                        tkns_sold += bid[0] * buy_tkns_remaining
+                        bid[1] -= buy_tkns_remaining
+                        buy_tkns_remaining = 0
+                    else:
+                        tkns_sold += bid[0] * bid[1]
+                        buy_tkns_remaining -= bid[1]
+                        bid[1] = 0
 
-            for ask in orders:
-                if buy_tkns_remaining <= 0:
-                    break
-                if ask[1] >= buy_tkns_remaining:
-                    # this ask can fill the entire remaining order
-                    tkns_sold += ask[0] * buy_tkns_remaining
-                    ask[1] -= buy_tkns_remaining
-                    buy_tkns_remaining = 0
-                else:
-                    # this ask can partially fill the order
-                    tkns_sold += ask[0] * ask[1]
-                    buy_tkns_remaining -= ask[1]
-                    orders.remove(ask)
-
-            agent.holdings[tkn_buy] -= buy_quantity - buy_tkns_remaining
-            agent.holdings[tkn_sell] += tkns_sold
+                agent.holdings[tkn_buy] += buy_quantity - buy_tkns_remaining
+                agent.holdings[tkn_sell] -= tkns_sold * (1 - self.trade_fee)
+                self.order_book[(base, quote)].bids = [bid for bid in self.order_book[(base, quote)].bids if bid[1] > 0]
 
         return self
 
