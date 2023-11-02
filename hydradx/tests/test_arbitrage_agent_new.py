4325fa3693f684b73f117ac1a6d4148a182b2c6b
poliwop <grove.colin@gmail.com>
2023-11-02 15:40:12 -0500

    Fixed references in tests

--- hydradx/tests/test_arbitrage_agent_new.py
+++ hydradx/tests/test_arbitrage_agent_new.py
@@ -3,38 +3,10 @@ from datetime import timedelta
 from hypothesis import given, strategies as st, settings, reproduce_failure, Verbosity, Phase
 
 from hydradx.model.amm.agents import Agent
-from hydradx.model.amm.arbitrage_agent_new import calculate_profit, calculate_arb_amount_bid, calculate_arb_amount_ask
-from hydradx.model.amm.arbitrage_agent_new import get_arb_swaps, execute_arb
+from hydradx.model.amm.arbitrage_agent import calculate_profit, execute_arb
+from hydradx.model.amm.arbitrage_agent_new import get_arb_swaps
 from hydradx.model.amm.omnipool_amm import OmnipoolState
 from hydradx.model.amm.centralized_market import OrderBook, CentralizedMarket
-
-
-def test_calculate_profit():
-    init_agent = Agent(holdings={'USDT': 1000000, 'DOT': 2000000, 'HDX': 3000000}, unique_id='bot')
-    agent = init_agent.copy()
-    profits = {'USDT': 100, 'DOT': 200, 'HDX': 0}
-    agent.holdings['USDT'] += profits['USDT']
-    agent.holdings['DOT'] += profits['DOT']
-    agent.holdings['HDX'] += profits['HDX']
-    calculated_profit = calculate_profit(init_agent, agent)
-    for tkn in init_agent.holdings:
-        assert calculated_profit[tkn] == profits[tkn]
-
-
-def test_calculate_profit_with_mapping():
-    init_agent = Agent(holdings={'USDT': 1000000, 'DOT': 2000000, 'HDX': 3000000, 'DAI': 4000000}, unique_id='bot')
-    agent = init_agent.copy()
-    profits = {'USDT': 100, 'DOT': 200, 'HDX': 0, 'DAI': 100}
-    agent.holdings['USDT'] += profits['USDT']
-    agent.holdings['DAI'] += profits['DAI']
-    agent.holdings['DOT'] += profits['DOT']
-    agent.holdings['HDX'] += profits['HDX']
-    mapping = {'DAI': 'USD', 'USDT': 'USD'}
-    calculated_profit = calculate_profit(init_agent, agent, mapping)
-
-    assert calculated_profit['USD'] == 200
-    assert calculated_profit['DOT'] == 200
-    assert calculated_profit['HDX'] == 0
 
 
 @given(
