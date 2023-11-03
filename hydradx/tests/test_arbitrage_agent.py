0c353cc00e689d86fa6390c7da04102eec7500f3
poliwop <grove.colin@gmail.com>
2023-11-03 15:30:15 -0500

    Added test_process_next_swap

--- hydradx/tests/test_arbitrage_agent.py
+++ hydradx/tests/test_arbitrage_agent.py
@@ -1,10 +1,12 @@
+import copy
 from datetime import timedelta
 import pytest
 
 from hypothesis import given, strategies as st, settings, reproduce_failure, Verbosity, Phase
 
 from hydradx.model.amm.agents import Agent
-from hydradx.model.amm.arbitrage_agent import calculate_profit, calculate_arb_amount_bid, calculate_arb_amount_ask
+from hydradx.model.amm.arbitrage_agent import calculate_profit, calculate_arb_amount_bid, calculate_arb_amount_ask, \
+    process_next_swap
 from hydradx.model.amm.arbitrage_agent import get_arb_swaps_simple, execute_arb, get_arb_swaps
 from hydradx.model.amm.omnipool_amm import OmnipoolState
 from hydradx.model.amm.centralized_market import OrderBook, CentralizedMarket
@@ -356,6 +358,146 @@ def test_calculate_arb_amount_ask_max_liquidity(
     profit = calculate_profit(init_agent, agent)
     for tkn in profit:
         assert profit[tkn] >= 0
+
+
+@given(
+    dotusd_price_mult=st.floats(min_value=0.8, max_value=1.2),
+    dot_amts=st.lists(st.floats(min_value=10, max_value=10000), min_size=8, max_size=8),
+    hdxdot_price_mult=st.floats(min_value=0.8, max_value=1.2),
+    hdxdot_amts=st.lists(st.floats(min_value=10, max_value=10000), min_size=4, max_size=4),
+    hdxusd_price_mult=st.floats(min_value=0.8, max_value=1.2),
+    hdxusd_amts=st.lists(st.floats(min_value=10, max_value=10000), min_size=8, max_size=8),
+)
+def test_process_next_swap(
+        dotusd_price_mult: float,
+        dot_amts: list,
+        hdxdot_price_mult: float,
+        hdxdot_amts: list,
+        hdxusd_price_mult: float,
+        hdxusd_amts: list
+):
+    tokens = {
+        'USDT': {
+            'liquidity': 2062772,
+            'LRNA': 2062772
+        },
+        'DOT': {
+            'liquidity': 350000,
+            'LRNA': 1456248
+        },
+        'HDX': {
+            'liquidity': 108000000,
+            'LRNA': 494896
+        }
+    }
+
+    lrna_fee = 0.0005
+    asset_fee = 0.0025
+    cex_fee = 0.0016
+
+    op_state = OmnipoolState(
+        tokens=tokens,
+        lrna_fee=lrna_fee,
+        asset_fee=asset_fee,
+        preferred_stablecoin='USDT',
+    )
+
+    dotusd_spot = op_state.price(op_state, 'DOT', 'USDT')
+    dotusd_spot_adj = dotusd_spot * dotusd_price_mult
+
+    dot_usdt_order_book = {
+        'bids': [{'price': dotusd_spot_adj * 0.999, 'amount': dot_amts[0]},
+                 {'price': dotusd_spot_adj * 0.99, 'amount': dot_amts[1]},
+                 {'price': dotusd_spot_adj * 0.9, 'amount': dot_amts[2]},
+                 {'price': dotusd_spot_adj * 0.8, 'amount': dot_amts[3]}],
+        'asks': [{'price': dotusd_spot_adj * 1.001, 'amount': dot_amts[4]},
+                 {'price': dotusd_spot_adj * 1.01, 'amount': dot_amts[5]},
+                 {'price': dotusd_spot_adj * 1.1, 'amount': dot_amts[6]},
+                 {'price': dotusd_spot_adj * 1.2, 'amount': dot_amts[7]}]
+    }
+
+    dot_usdt_order_book_obj = OrderBook([[bid['price'], bid['amount']] for bid in dot_usdt_order_book['bids']],
+                                        [[ask['price'], ask['amount']] for ask in dot_usdt_order_book['asks']])
+
+    hdxusd_spot = op_state.price(op_state, 'HDX', 'USDT')
+    hdxusd_spot_adj = hdxusd_spot * hdxusd_price_mult
+
+    hdx_usdt_order_book = {
+        'bids': [{'price': hdxusd_spot_adj * 0.999, 'amount': hdxusd_amts[0]},
+                 {'price': hdxusd_spot_adj * 0.99, 'amount': hdxusd_amts[1]},
+                 {'price': hdxusd_spot_adj * 0.9, 'amount': hdxusd_amts[2]},
+                 {'price': hdxusd_spot_adj * 0.8, 'amount': hdxusd_amts[3]}],
+        'asks': [{'price': hdxusd_spot_adj * 1.001, 'amount': hdxusd_amts[4]},
+                 {'price': hdxusd_spot_adj * 1.01, 'amount': hdxusd_amts[5]},
+                 {'price': hdxusd_spot_adj * 1.1, 'amount': hdxusd_amts[6]},
+                 {'price': hdxusd_spot_adj * 1.2, 'amount': hdxusd_amts[7]}]
+    }
+
+    hdx_usdt_order_book_obj = OrderBook([[bid['price'], bid['amount']] for bid in hdx_usdt_order_book['bids']],
+                                        [[ask['price'], ask['amount']] for ask in hdx_usdt_order_book['asks']])
+
+    hdxdot_spot = op_state.price(op_state, 'HDX', 'DOT')
+    hdxdot_spot_adj = hdxdot_spot * hdxdot_price_mult
+
+    hdx_dot_order_book = {
+        'bids': [{'price': hdxdot_spot_adj * 0.99, 'amount': hdxdot_amts[0]},
+                 {'price': hdxdot_spot_adj * 0.9, 'amount': hdxdot_amts[1]}],
+        'asks': [{'price': hdxdot_spot_adj * 1.01, 'amount': hdxdot_amts[2]},
+                 {'price': hdxdot_spot_adj * 1.1, 'amount': hdxdot_amts[3]}]
+    }
+
+    hdx_dot_order_book_obj = OrderBook([[bid['price'], bid['amount']] for bid in hdx_dot_order_book['bids']],
+                                        [[ask['price'], ask['amount']] for ask in hdx_dot_order_book['asks']])
+
+    order_book = {
+        ('DOT', 'USDT'): dot_usdt_order_book_obj,
+        ('HDX', 'USDT'): hdx_usdt_order_book_obj,
+        ('HDX','DOT'): hdx_dot_order_book_obj
+    }
+
+    cex = CentralizedMarket(
+        order_book=order_book,
+        asset_list=['USDT', 'DOT', 'HDX'],
+        trade_fee=cex_fee
+    )
+
+    # get_arb_swaps(op_state, cex, order_book_map, buffer=0.0, max_trades={}, iters=20)
+
+    order_book_map = {k: k for k in order_book}
+
+    agent = Agent(holdings={'USDT': 1000000000, 'DOT': 1000000000, 'HDX': 1000000000}, unique_id='bot')
+
+    test_state = op_state.copy()
+    test_agent = agent.copy()
+    test_cex = cex.copy()
+    buffer = 0.0
+    init_max_liquidity = {'cex': {'USDT': 1000, 'DOT': 100, 'HDX': 100000}, 'dex': {'USDT': 1000, 'DOT': 100, 'HDX': 100000}}
+    max_liquidity = copy.deepcopy(init_max_liquidity)
+    iters = 20
+    tkn_pair = ('DOT', 'USDT')
+
+    swap = process_next_swap(test_state, test_agent, test_cex, tkn_pair, order_book_map, buffer, max_liquidity, iters)
+    if swap:
+        arb_swaps = [swap]
+
+        initial_agent = Agent(holdings={'USDT': 1000000000, 'DOT': 1000000000, 'HDX': 1000000000}, unique_id='bot')
+        agent = initial_agent.copy()
+
+        execute_arb(op_state, cex, agent, arb_swaps)
+
+        profit = calculate_profit(initial_agent, agent)
+        for tkn in profit:
+            if profit[tkn] / initial_agent.holdings[tkn] < -1e-10:
+                raise
+
+        for tkn in op_state.asset_list:
+            if tkn in max_liquidity:
+                if test_state.liquidity[tkn] - op_state.liquidity[tkn] != init_max_liquidity['dex'][tkn] - max_liquidity['dex'][tkn]:
+                    raise
+        for tkn in cex.asset_list:
+            if tkn in max_liquidity:
+                if test_cex.liquidity[tkn] - cex.liquidity[tkn] != init_max_liquidity['cex'][tkn] - max_liquidity['cex'][tkn]:
+                    raise
 
 
 @given(
