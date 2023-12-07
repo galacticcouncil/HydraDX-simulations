f746c4eb56cda6b9dc42a8cbd35c5f5cd8f8230b
jepidoptera <jepidoptera@live.com>
2023-12-07 13:44:02 +0100

    added test_buy_spot and test_sell_spot

--- hydradx/tests/test_stableswap.py
+++ hydradx/tests/test_stableswap.py
@@ -585,3 +585,95 @@ def test_amplification_change_exploit():  # (end_amp):
     loss = sum(initial_pool.liquidity.values()) - sum(final_pool.liquidity.values())
     if loss > 0:
         raise AssertionError(F"Pool lost money. loss: {round(loss / sum(initial_pool.liquidity.values()) * 100, 5)}%")
+
+
+@given(
+    liquidity_stepdown=st.integers(min_value=-1000000, max_value=1000000),
+    assets_number=st.integers(min_value=2, max_value=4),
+    amplification=st.integers(min_value=1, max_value=10000),
+    trade_fee=st.floats(min_value=0, max_value=0.1)
+)
+def test_sell_spot(
+        liquidity_stepdown: int,
+        assets_number: int,
+        amplification: int,
+        trade_fee: float
+):
+    base_liquidity = mpf(10000000)
+    initial_state = StableSwapPoolState(
+        tokens={
+            tkn: base_liquidity + liquidity_stepdown * n
+            for n, tkn in enumerate(['R' + str(n) for n in range(1, assets_number + 1)])
+        },
+        amplification=amplification,
+        trade_fee=trade_fee
+    )
+    tkn_sell = initial_state.asset_list[0]
+    tkn_buy = initial_state.asset_list[1]
+    initial_agent = Agent(
+        holdings={tkn: 1000 for tkn in initial_state.asset_list}
+    )
+    sell_quantity = 0.001
+    sell_state = initial_state.copy()
+    sell_agent = initial_agent.copy()
+    sell_spot_1 = initial_state.sell_spot(tkn_buy=tkn_buy, tkn_sell=tkn_sell, tkn_buy_is_numeraire=False)
+    sell_spot_2 = initial_state.sell_spot(tkn_buy=tkn_buy, tkn_sell=tkn_sell, tkn_buy_is_numeraire=True)
+    sell_state.swap(
+        agent=sell_agent,
+        tkn_sell=tkn_sell,
+        tkn_buy=tkn_buy,
+        sell_quantity=sell_quantity
+    )
+    buy_quantity = sell_agent.holdings[tkn_buy] - initial_agent.holdings[tkn_buy]
+    ex_price_1 = sell_quantity / buy_quantity
+    ex_price_2 = buy_quantity / sell_quantity
+    if sell_spot_1 != pytest.approx(ex_price_1):
+        raise AssertionError('Sell spot price incorrect.')
+    if sell_spot_2 != pytest.approx(ex_price_2):
+        raise AssertionError('Sell spot price incorrect.')
+
+
+@given(
+    liquidity_stepdown=st.integers(min_value=-1000000, max_value=1000000),
+    assets_number=st.integers(min_value=2, max_value=4),
+    amplification=st.integers(min_value=1, max_value=10000),
+    trade_fee=st.floats(min_value=0, max_value=0.1)
+)
+def test_buy_spot(
+        liquidity_stepdown: int,
+        assets_number: int,
+        amplification: int,
+        trade_fee: float
+):
+    base_liquidity = mpf(10000000)
+    initial_state = StableSwapPoolState(
+        tokens={
+            tkn: base_liquidity + liquidity_stepdown * n
+            for n, tkn in enumerate(['R' + str(n) for n in range(1, assets_number + 1)])
+        },
+        amplification=amplification,
+        trade_fee=trade_fee
+    )
+    tkn_sell = initial_state.asset_list[0]
+    tkn_buy = initial_state.asset_list[1]
+    initial_agent = Agent(
+        holdings={tkn: 1000 for tkn in initial_state.asset_list}
+    )
+    buy_quantity = 0.001
+    buy_state = initial_state.copy()
+    buy_agent = initial_agent.copy()
+    buy_spot_1 = initial_state.buy_spot(tkn_buy=tkn_buy, tkn_sell=tkn_sell, tkn_sell_is_numeraire=False)
+    buy_spot_2 = initial_state.buy_spot(tkn_buy=tkn_buy, tkn_sell=tkn_sell, tkn_sell_is_numeraire=True)
+    buy_state.swap(
+        agent=buy_agent,
+        tkn_sell=tkn_sell,
+        tkn_buy=tkn_buy,
+        buy_quantity=buy_quantity
+    )
+    sell_quantity = initial_agent.holdings[tkn_sell] - buy_agent.holdings[tkn_sell]
+    ex_price_1 = buy_quantity / sell_quantity
+    ex_price_2 = sell_quantity / buy_quantity
+    if buy_spot_1 != pytest.approx(ex_price_1):
+        raise AssertionError('Buy spot price incorrect.')
+    if buy_spot_2 != pytest.approx(ex_price_2):
+        raise AssertionError('Buy spot price incorrect.')
