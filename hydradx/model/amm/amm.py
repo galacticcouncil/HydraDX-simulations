20c78a6400bfc6922c9ffebeb80ee86b1d6fea5f
jepidoptera <jepidoptera@live.com>
2023-12-11 12:37:57 +0100

    changed buy_spot and sell_spot template definitions

--- hydradx/model/amm/amm.py
+++ hydradx/model/amm/amm.py
@@ -46,10 +46,16 @@ class AMM:
     def price(state, tkn: str, denomination: str = '') -> float:
         return 0
 
-    def buy_spot(self, tkn_buy: str, tkn_sell: str, tkn_sell_is_numeraire: bool = False) -> float:
+    def buy_spot(self, tkn_buy: str, tkn_sell: str) -> float:
+        """
+        How much tkn_sell will 1 tkn_buy cost?
+        """
         return 0
 
-    def sell_spot(self, tkn_buy: str, tkn_sell: str, tkn_buy_is_numeraire: bool = False) -> float:
+    def sell_spot(self, tkn_sell: str, tkn_buy: str) -> float:
+        """
+        How much tkn_buy can be bought for 1 tkn_sell?
+        """
         return 0
 
     def buy_limit(self, tkn_buy, tkn_sell):
