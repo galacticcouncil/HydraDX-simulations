9499f68771b6e3c999eb0797abb4b36d373a22e6
jepidoptera <jepidoptera@live.com>
2024-05-02 10:55:17 -0500

    switch tkn_buy and tkn_sell fees in spot price functions

--- hydradx/model/amm/omnipool_amm.py
+++ hydradx/model/amm/omnipool_amm.py
@@ -357,9 +357,9 @@ class OmnipoolState(AMM):
                         fee['asset'] = pool.trade_fee
                         break
             if 'lrna' not in fee:
-                fee['lrna'] = self.lrna_fee[tkn_sell].compute()
+                fee['lrna'] = self.lrna_fee[tkn_buy].compute()
             if 'asset' not in fee:
-                fee['asset'] = self.asset_fee[tkn_buy].compute()
+                fee['asset'] = self.asset_fee[tkn_sell].compute()
         elif not isinstance(fee, dict):
             fee = {
                 'lrna': fee,
@@ -392,9 +392,9 @@ class OmnipoolState(AMM):
                         fee['asset'] = pool.trade_fee
                         break
             if 'lrna' not in fee:
-                fee['lrna'] = self.lrna_fee[tkn_sell].compute()
+                fee['lrna'] = self.lrna_fee[tkn_buy].compute()
             if 'asset' not in fee:
-                fee['asset'] = self.asset_fee[tkn_buy].compute()
+                fee['asset'] = self.asset_fee[tkn_sell].compute()
         elif not isinstance(fee, dict):
             fee = {
                 'lrna': fee,
