635f8056428c73d79bcc3066d62f4ba7ba261f26
jepidoptera <jepidoptera@live.com>
2024-05-07 13:14:41 +0200

    test with no fees - good

--- hydradx/tests/test_concentrated_liquidity.py
+++ hydradx/tests/test_concentrated_liquidity.py
@@ -12,21 +12,22 @@ token_amounts = st.floats(min_value=0.01, max_value=1000.0, allow_nan=False, all
 price_strategy = st.floats(min_value=0.1, max_value=10.0, allow_nan=False, allow_infinity=False)
 fee_strategy = st.floats(min_value=0.0, max_value=0.05, allow_nan=False, allow_infinity=False)
 
-@given(price_strategy, fee_strategy)
-def test_price_boundaries(initial_price, trade_fee):
-    tick_spacing = 10
+@given(price_strategy)
+def test_price_boundaries_no_fee(initial_price):
+    tick_spacing = 1
     price_spread = 10
     price = tick_to_price(price_to_tick(initial_price, tick_spacing=tick_spacing))
     initial_state = ConcentratedLiquidityState(
-        assets={'A': 1000 / price, 'B': 1000},
+        assets={'A': mpf(1000 / price), 'B': mpf(1000)},
         min_tick=price_to_tick(price) - price_spread,
         tick_spacing=tick_spacing,
-        fee=trade_fee
+        fee=0
     )
     second_state = ConcentratedLiquidityState(
         assets=initial_state.liquidity.copy(),
         max_tick=initial_state.max_tick,
-        fee=trade_fee
+        tick_spacing=tick_spacing,
+        fee=0
     )
     if initial_state.min_tick != second_state.min_tick:
         raise AssertionError('min_tick is not the same')
@@ -35,12 +36,12 @@ def test_price_boundaries(initial_price, trade_fee):
         buy_x_agent, tkn_buy='A', tkn_sell='B', buy_quantity=initial_state.liquidity['A']
     )
     new_price = buy_x_state.price('A')
-    # if new_price != pytest.approx(initial_state.max_price, rel=1e-08):
-    #     raise AssertionError(f"Buying all initial liquidity[A] should raise price to max.")
+    if new_price != pytest.approx(initial_state.max_price, rel=1e-20):
+        raise AssertionError(f"Buying all initial liquidity[A] should raise price to max.")
     buy_y_agent = Agent(holdings={'A': 100000})
     buy_y_state = initial_state.copy().swap(
         buy_y_agent, tkn_buy='B', tkn_sell='A', buy_quantity=initial_state.liquidity['B']
     )
     new_price = buy_y_state.price('A')
-    if new_price != pytest.approx(initial_state.min_price, rel=1e-08):
+    if new_price != pytest.approx(initial_state.min_price, rel=1e-20):
         raise AssertionError(f"Buying all initial liquidity[B] should lower price to min.")
