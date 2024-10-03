79fe743a3ff84dcea6abd09f5d9f848619bef017
poliwop <grove.colin@gmail.com>
2024-10-03 11:05:45 -0500

    refactor

---
+++ hydradx/model/amm/omnix_solver_simple.py
@@ -0,0 +1,91 @@
+import numpy as np
+import cvxpy as cp
+
+from hydradx.model.amm.omnipool_amm import OmnipoolState
+
+
+def convert_intents(intents, tkn_list):
+    tkn_map = {tkn: i for i, tkn in enumerate(tkn_list)}
+
+    intent_indices = []
+    intent_reserves = []
+    intent_prices = []
+    for intent in intents:
+        intent_indices.append([tkn_map[intent['tkn_sell']], tkn_map[intent['tkn_buy']]])
+        sell_amt = intent['sell_quantity']
+        buy_amt = intent['buy_quantity']
+        intent_reserves.append([sell_amt, 0])
+        intent_prices.append(buy_amt / sell_amt)
+
+    return intent_indices, intent_reserves, intent_prices
+
+
+def find_solution(state: OmnipoolState, intents: list):
+
+    tkn_list = ["LRNA"] + state.asset_list
+
+    reserves_list = [[state.lrna[tkn], state.liquidity[tkn]] for tkn in state.lrna]
+
+    fees = [0.003] * 3
+
+    profit_value = [1, 0, 0, 0]  # for taking profits in LRNA
+
+    # transform data for cvxpy
+    intent_indices, intent_reserves, intent_prices = convert_intents(intents, tkn_list)
+    global_indices = list(range(len(tkn_list)))
+    local_indices = [[0, i+1] for i in range(len(state.asset_list))]
+
+    reserves = list(map(np.array, reserves_list))
+    reserves2 = list(map(np.array, intent_reserves))
+
+    # Build local-global matrices
+    n = len(global_indices)
+
+    A = []
+    for l in local_indices + intent_indices:
+        n_i = len(l)
+        A_i = np.zeros((n, n_i))
+        for i, idx in enumerate(l):
+            A_i[idx, i] = 1
+        A.append(A_i)
+
+    # Build variables
+    deltas = [cp.Variable(len(l), nonneg=False) for l in local_indices]
+    intent_deltas = [cp.Variable(len(l), nonneg=False) for l in intent_indices]
+
+    profits = [A_i @ (-D) for A_i, D, in zip(A, deltas + intent_deltas)]  # assets from AMMs
+
+    psi = cp.sum(profits)
+
+    # Objective is to maximize "total market value" of coins out
+    obj = cp.Maximize(profit_value @ psi)  # take profits in LRNA
+
+    # Reserves after trade
+    new_reserves = [R + (1 - f_i) * D for R, f_i, D in zip(reserves, fees, deltas)]
+    new_intent_reserves = [R + D for R, D in zip(reserves2, intent_deltas)]
+
+
+    cons = [psi >= 0]  # no negative profits
+    for i in range(len(reserves)):  # AMM invariants must not go down
+        cons.append(cp.geo_mean(new_reserves[i]) >= cp.geo_mean(reserves[i]))  # this doesn't account for fees correctly
+
+    for i in range(len(intent_indices)):  # intent constraints
+        cons.append(intent_prices[i] * intent_deltas[i][0] + intent_deltas[i][1] == 0)  # sale must be at set price
+        cons.append(intent_deltas[i][0] <= 0)  # cannot buy the sell asset
+        cons.append(new_intent_reserves[i][0] >= 0)  # cannot sell more than you have
+
+    # Set up and solve problem
+    prob = cp.Problem(obj, cons)
+    prob.solve(verbose=True)
+
+    # extract solution
+    amm_ct = len(local_indices)
+    amm_deltas = [None] * amm_ct
+    intent_deltas = [None] * len(intent_indices)
+    for i in (prob.solution.primal_vars):
+        if i-1 < amm_ct:  # AMM deltas
+            amm_deltas[i-1] = prob.solution.primal_vars[i]
+        else:  # intent deltas
+            intent_deltas[i-1 - amm_ct] = prob.solution.primal_vars[i]
+
+    return amm_deltas, intent_deltas
