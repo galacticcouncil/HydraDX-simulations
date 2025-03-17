d17b1addd0d6758999e7dbe659a1cf6eb1609770
jepidoptera <jepidoptera@live.com>
2025-03-17 18:26:59 -0500

    dummy CEX class with infinite liquidity

---
+++ hydradx/model/amm/infinite_liquidity.py
@@ -0,0 +1,57 @@
+from .exchange import Exchange
+from .agents import Agent
+
+class CEXDummy(Exchange):
+    def __init__(self, tokens: dict[str: float], unique_id='dummy exchange'):
+        """
+        Mock exchange with infinite liquidity and no fees
+        """
+        super().__init__()
+        self.prices = tokens
+        self.liquidity = {tkn: 0 for tkn in tokens}
+        self.asset_list = list(tokens.keys())
+        self.unique_id = unique_id
+
+    def price(self, tkn: str, denomination: str = ''):
+        if denomination and denomination not in self.prices:
+            raise ValueError(f'Denomination {denomination} not in exchange')
+        return self.prices[tkn] / (self.prices[denomination] if denomination in self.prices else 1)
+
+    def buy_spot(self, tkn_buy, tkn_sell, fee=0):
+        return self.price(tkn_buy) / self.price(tkn_sell)
+
+    def sell_spot(self, tkn_sell, tkn_buy, fee=0):
+        return self.price(tkn_sell) / self.price(tkn_buy)
+
+    def buy_limit(self, tkn_buy, tkn_sell):
+        return float('inf')
+
+    def sell_limit(self, tkn_buy, tkn_sell):
+        return float('inf')
+
+    def swap(
+            self,
+            agent: Agent,
+            tkn_buy: str,
+            tkn_sell: str,
+            buy_quantity: float = 0,
+            sell_quantity: float = 0
+    ):
+        if buy_quantity:
+            sell_quantity = self.calculate_sell_from_buy(tkn_buy, tkn_sell, buy_quantity)
+        elif sell_quantity:
+            buy_quantity = self.calculate_buy_from_sell(tkn_buy, tkn_sell, sell_quantity)
+
+        if tkn_buy not in agent.holdings:
+            agent.holdings[tkn_buy] = 0
+
+        agent.holdings[tkn_sell] -= sell_quantity
+        agent.holdings[tkn_buy] += buy_quantity
+        self.liquidity[tkn_sell] += sell_quantity
+        self.liquidity[tkn_buy] -= buy_quantity
+
+    def calculate_buy_from_sell(self, tkn_buy, tkn_sell, sell_quantity):
+        return sell_quantity * self.price(tkn_sell) / self.price(tkn_buy)
+
+    def calculate_sell_from_buy(self, tkn_sell, tkn_buy, buy_quantity):
+        return buy_quantity * self.price(tkn_buy) / self.price(tkn_sell)
