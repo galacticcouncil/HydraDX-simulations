df47ae2854fb8f8a2f46d076b038fa074cf9f7c9
poliwop <grove.colin@gmail.com>
2025-03-18 11:02:18 -0500

    revert unnecessary changes

--- hydradx/model/amm/omnipool_router.py
+++ hydradx/model/amm/omnipool_router.py
@@ -1,8 +1,8 @@
 import copy
 
-from .agents import Agent
-from .omnipool_amm import OmnipoolState
-from .stableswap_amm import StableSwapPoolState
+from hydradx.model.amm.agents import Agent
+from hydradx.model.amm.omnipool_amm import OmnipoolState
+from hydradx.model.amm.stableswap_amm import StableSwapPoolState
 
 class OmnipoolRouter:
     """
@@ -337,17 +337,7 @@ class OmnipoolRouter:
         if len(tkn_sell_pools) == 0:
             raise ValueError(f'No pool with {tkn_sell} in asset list')
 
-        routes = []
-        for tkn_buy_pool in tkn_buy_pools:
-            if tkn_buy_pool not in self.omnipool.asset_list + [self.omnipool_id]:
-                if tkn_sell in self.exchanges[tkn_buy_pool].asset_list:
-                    routes.append((tkn_buy_pool, tkn_buy_pool))
-            else:
-                for tkn_sell_pool in tkn_sell_pools:
-                    if tkn_sell_pool in self.omnipool.asset_list + [self.omnipool_id]:
-                        routes.append((tkn_sell_pool, tkn_buy_pool))
-
-        return routes
+        return [(tkn_sell_pool, tkn_buy_pool) for tkn_buy_pool in tkn_buy_pools for tkn_sell_pool in tkn_sell_pools]
 
     def find_best_route(self, tkn_buy, tkn_sell) -> tuple[str, str]:
         """
