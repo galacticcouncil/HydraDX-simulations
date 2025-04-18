625a4e27c2211e05ab5b2e52e247d997223b375c
poliwop <grove.colin@gmail.com>
2025-04-18 09:05:14 -0500

    WIP pulling stableswap data

--- hydradx/apps/gigadot_modeling/gigadot_live.py
+++ hydradx/apps/gigadot_modeling/gigadot_live.py
@@ -7,6 +7,7 @@ sys.path.append(project_root)
 from hydradx.model.indexer_utils import get_latest_stableswap_data
 from hydradx.model.amm.stableswap_amm import StableSwapPoolState
 from hydradx.apps.gigadot_modeling.display_utils import display_ss
+from hydradx.model.amm.agents import Agent
 
 pool_id = 102  # 2-pool, for now
 pool_data = get_latest_stableswap_data(pool_id)
@@ -19,14 +20,12 @@ prices = {"USDT": 1, "USDC": 1}  # Dummy prices for display
 fig = display_ss(pool.liquidity, prices, "")
 st.sidebar.pyplot(fig)
 
-# self,
-# tokens: dict,
-# amplification: float,
-# precision: float = 0.0001,
-# trade_fee: float = 0,
-# unique_id: str = '',
-# spot_price_precision: float = 1e-07,
-# shares: float = 0,
-# peg: float or list = None,
-# peg_target: float or list = None,
-# max_peg_update: float = float('inf')
+usdt_buy_size = st.number_input(
+    "USDT buy size",
+    min_value=1.0, max_value=1000000.0, value=10000.0, step=1.0, key="usdt_buy_size", format="%.2f"
+)
+
+agent = Agent(enforce_holdings=False)
+pool.swap(agent, tkn_buy='USDT', tkn_sell='USDC', buy_quantity=usdt_buy_size)
+sell_amt = -agent.get_holdings('USDC')
+st.text(f"sell amt: {sell_amt}")
