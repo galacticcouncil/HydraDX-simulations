f0103496a3a3d700188449fcc0e79773ec56be27
poliwop <grove.colin@gmail.com>
2025-05-02 10:38:04 -0500

    fixed some tests

--- hydradx/tests/test_stableswap.py
+++ hydradx/tests/test_stableswap.py
@@ -1074,11 +1074,10 @@ def test_fuzz_arb_repegging_lp(fee, balance_pct, amp, repeg_pct, max_repeg):
 @given(
     st.floats(min_value=0.01, max_value=100),
     st.floats(min_value=0.01, max_value=100),
-    st.floats(min_value=10, max_value=100000),
+    st.floats(min_value=10, max_value=10000),
     st.floats(min_value=-0.5, max_value=0.5, exclude_min=True),
     st.floats(min_value=-0.5, max_value=0.5, exclude_min=True)
 )
-# @settings(max_examples=10000)
 def test_fuzz_arb_repegging_3pool(
         ratio1,
         ratio2,
@@ -1092,7 +1091,7 @@ def test_fuzz_arb_repegging_3pool(
     fee = 0.0001
     max_repeg = 0.001
 
-    dot_liq = 1000000
+    dot_liq = mpf(1000000)
     tokens = {
         'DOT': dot_liq,
         'vDOT': ratio1 * dot_liq / init_vDOT_price,
@@ -1107,7 +1106,7 @@ def test_fuzz_arb_repegging_3pool(
 
             pool = StableSwapPoolState(copy.deepcopy(tokens), amp, trade_fee=fee,
                                        peg=[init_vDOT_price, init_lstDOT_price], max_peg_update=max_repeg,
-                                       precision=0.000001)
+                                       precision=0.0000000001)
             pool.swap(agent, tkn_sell, tkn_buy, sell_quantity=arb_size)
             pool.set_peg_target(peg_target)
             pool.swap(agent, tkn_buy, tkn_sell, sell_quantity=agent.holdings[tkn_buy])
